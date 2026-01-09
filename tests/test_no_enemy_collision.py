3764d378652b0e0fd06645ed6ed07fac67ed2827
SuperMeister808 <emil.chatzistamatiou@gmail.com>
2026-01-09 18:06:17 +0100

    Feature Added: Priority Moves

--- tests/test_no_enemy_collision.py
+++ tests/test_no_enemy_collision.py
@@ -8,7 +8,8 @@ class TestNoEnemyCollision(unittest.TestCase):
     def test_priority_left(self):
 
         game_state = {"you": {"id": "my", "head": {"x": 2, "y": 2} ,"body": [{"x": 2, "y": 2}, {"x": 2, "y": 3}, {"x": 2, "y": 4}],"length": 3}, 
-                      "board": {"snakes": {"id": "opponent", "head": {"x": 1, "y": 2}, "body": [{"x": 1, "y": 2}, {"x": 1, "y": 3}, {"x": 1, "y": 4}]}}
+                      "board": {"snakes": [{"id": "opponent", "head": {"x": 0, "y": 2}, "body": [{"x": 0, "y": 2}, {"x": 0, "y": 3}], "length": 2}], "food": [{"x": 10, "y": 10}]},
+                      "turn": 1
                       }
         
         bot = Move()
@@ -28,6 +29,133 @@ class TestNoEnemyCollision(unittest.TestCase):
                     expectation = {"move": "left"}
 
                     self.assertEqual(result, expectation)
+
+    def test_priority_up(self):
+
+        game_state = {"you": {"id": "my", "head": {"x": 1, "y": 2} ,"body": [{"x": 1, "y": 2}, {"x": 2, "y": 2}, {"x": 3, "y": 2}],"length": 3}, 
+                      "board": {"snakes": [{"id": "opponent", "head": {"x": 1, "y": 4}, "body": [{"x": 1, "y": 4}, {"x": 2, "y": 4}], "length": 2}], "food": [{"x": 10, "y": 10}]},
+                      "turn": 1
+                      }
+        
+        with patch.object(Move, "not_backward", return_value = "Patch not_backward") as patch_1:
+
+            with patch.object(Move, "not_wall_collision", return_value = "patch not_wall_collision") as patch_2:
+
+                with patch.object(Move, "not_itself_collision", return_value = "patch not_itself_collision") as patch_3:
+
+                    bot = Move()
+                    
+                    result = bot.choose_move(game_state)
+                    
+                    patch_1.assert_called_once()
+                    patch_2.assert_called_once()
+                    patch_3.assert_called_once()
+
+                    expectation = {"move": "up"}
+
+                    self.assertEqual(result, expectation)
+
+    def test_multiple_snakes(self):
+
+        game_state = {"you": {"id": "my", "head": {"x": 1, "y": 2} ,"body": [{"x": 1, "y": 2}, {"x": 2, "y": 2}, {"x": 3, "y": 2}],"length": 3}, 
+                      "board": {"snakes": [{"id": "not_important", "head": {"x": 8, "y": 8}, "body": [{"x": 8, "y": 8}, {"x": 9, "y": 8}], "length": 2}, {"id": "opponent", "head": {"x": 1, "y": 4}, "body": [{"x": 1, "y": 4}, {"x": 2, "y": 4}], "length": 2}], "food": [{"x": 10, "y": 10}]},
+                      "turn": 1
+                      }
+        
+        with patch.object(Move, "not_backward", return_value = "Patch not_backward") as patch_1:
+
+            with patch.object(Move, "not_wall_collision", return_value = "patch not_wall_collision") as patch_2:
+
+                with patch.object(Move, "not_itself_collision", return_value = "patch not_itself_collision") as patch_3:
+
+                    bot = Move()
+                    
+                    result = bot.choose_move(game_state)
+                    
+                    patch_1.assert_called_once()
+                    patch_2.assert_called_once()
+                    patch_3.assert_called_once()
+
+                    expectation = {"move": "up"}
+
+                    self.assertEqual(result, expectation)
+
+    def test_no_priority(self):
+
+        game_state = {"you": {"id": "my", "head": {"x": 1, "y": 2} ,"body": [{"x": 1, "y": 2}, {"x": 2, "y": 2}, {"x": 3, "y": 2}],"length": 3}, 
+                      "board": {"snakes": [{"id": "opponent", "head": {"x": 8, "y": 8}, "body": [{"x": 8, "y": 8}, {"x": 9, "y": 8}], "length": 2}], "food": [{"x": 10, "y": 10}]},
+                      "turn": 1
+                      }
+        
+        with patch.object(Move, "not_backward", return_value = "Patch not_backward") as patch_1:
+
+            with patch.object(Move, "not_wall_collision", return_value = "patch not_wall_collision") as patch_2:
+
+                with patch.object(Move, "not_itself_collision", return_value = "patch not_itself_collision") as patch_3:
+
+                    bot = Move()
+                    
+                    result = bot.choose_move(game_state)
+                    
+                    patch_1.assert_called_once()
+                    patch_2.assert_called_once()
+                    patch_3.assert_called_once()
+
+                    expected_moves = ["up", "down", "left", "right"]
+
+                    move = result["move"]
+
+                    self.assertIn(move, expected_moves)
+
+    def test_is_growing(self):
+
+        game_state = {"you": {"id": "my", "head": {"x": 1, "y": 1} ,"body": [{"x": 1, "y": 1}, {"x": 1, "y": 2}, {"x": 1, "y": 3}],"length": 3}, 
+                      "board": {"snakes": [{"id": "opponent", "head": {"x": 3, "y": 1}, "body": [{"x": 3, "y": 1}, {"x": 2, "y": 1}], "length": 2}], "food": [{"x": 4, "y": 1}]},
+                      "turn": 1
+                      }
+        
+        with patch.object(Move, "not_backward", return_value = "Patch not_backward") as patch_1:
+
+            with patch.object(Move, "not_wall_collision", return_value = "patch not_wall_collision") as patch_2:
+
+                with patch.object(Move, "not_itself_collision", return_value = "patch not_itself_collision") as patch_3:
+
+                    bot = Move()
+                    
+                    result = bot.choose_move(game_state)
+                    
+                    patch_1.assert_called_once()
+                    patch_2.assert_called_once()
+                    patch_3.assert_called_once()
+
+                    is_safe = bot.is_move_safe["right"]["is_safe"]
+                    
+                    self.assertFalse(is_safe)
+
+    def test_is_not_growing(self):
+
+        game_state = {"you": {"id": "my", "head": {"x": 1, "y": 1} ,"body": [{"x": 1, "y": 1}, {"x": 1, "y": 2}, {"x": 1, "y": 3}],"length": 3}, 
+                      "board": {"snakes": [{"id": "opponent", "head": {"x": 3, "y": 1}, "body": [{"x": 3, "y": 1}, {"x": 2, "y": 1}], "length": 2}], "food": [{"x": 10, "y": 10}]},
+                      "turn": 1
+                      }
+        
+        with patch.object(Move, "not_backward", return_value = "Patch not_backward") as patch_1:
+
+            with patch.object(Move, "not_wall_collision", return_value = "patch not_wall_collision") as patch_2:
+
+                with patch.object(Move, "not_itself_collision", return_value = "patch not_itself_collision") as patch_3:
+
+                    bot = Move()
+                    
+                    result = bot.choose_move(game_state)
+                    
+                    patch_1.assert_called_once()
+                    patch_2.assert_called_once()
+                    patch_3.assert_called_once()
+
+                    is_safe = bot.is_move_safe["right"]["is_safe"]
+                    
+                    self.assertTrue(is_safe)
 
 if __name__ == "__main__":
 
