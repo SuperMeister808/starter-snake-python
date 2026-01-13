06c4c4475005d30b06533737264de877469abb71
SuperMeister808 <emil.chatzistamatiou@gmail.com>
2026-01-13 13:53:15 +0100

    Test passed!

--- tests/test_validate_game_state.py
+++ tests/test_validate_game_state.py
@@ -8,13 +8,27 @@ class TestValidateGameState(unittest.TestCase):
 
     def test_correct_game_state(self):
 
-        game_state = {"game": {}, "ruleset": {}, "squad": {}, "turn": {}, "board": {}, "you": {}}
+        game_state = {"game": {}, "ruleset": {}, "squad": {}, "turn": 1, "board": {}, "you": {}}
 
         self.assertTrue(validate_game_state(game_state))
 
     def test_wrong_key(self):
 
-        game_state = {"game": {}, "rules": {}, "squad": {}, "turn": {}, "board": {}, "you": {}}
+        game_state = {"game": {}, "rules": {}, "squad": {}, "turn": 1, "board": {}, "you": {}}
+
+        self.assertFalse(validate_game_state(game_state))
+
+    def test_wrong_data(self):
+
+        game_state = {"game": {}, "ruleset": {}, "squad": {}, "turn": {}, "board": {}, "you": {}}
+
+        self.assertFalse(validate_game_state(game_state))
+
+    def test_wrong_key_and_wrong_data(self):
+
+        game_state = {"gamer": {}, "ruleset": 1, "squad": {}, "turn": 1, "board": {}, "you": {}}
+
+        self.assertFalse(validate_game_state(game_state))
 
 if __name__ == "__main__":
 
