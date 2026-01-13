d6ee244c4e0a989af39bad8a0a6826c4376cc13a
SuperMeister808 <emil.chatzistamatiou@gmail.com>
2026-01-13 15:02:12 +0100

    Refactored run_server to Server()

--- server.py
+++ server.py
@@ -8,49 +8,62 @@ from flask import request
 from validate_game_state import validate_game_state
 
 
-def run_server(handlers: typing.Dict, port):
-    app = Flask("Battlesnake")
-    
-    @app.get("/")
-    def on_info():
-        return handlers["info"]()
+class Server():
 
-    @app.post("/start")
-    def on_start():
-        game_state = request.get_json()
-        if validate_game_state(game_state):
-            handlers["start"](game_state)
-            return "ok"
-        else:
-            return "Game State Validation Failed!" , 400
+    def __init__(self, handlers: typing.Dict, port):
 
-    @app.post("/move")
-    def on_move():
-        game_state = request.get_json()
-        if validate_game_state(game_state):
-            return handlers["move"](game_state)
-        else:
-            return "Game State Validation Failed!" , 400
+        self.app = Flask("Battlesnake")
+        
+        self.handlers = handlers
 
-    @app.post("/end")
-    def on_end():
-        game_state = request.get_json()
-        if validate_game_state(game_state):
-            handlers["end"](game_state)
-            return "ok"
-        else:
-            return "Game State Validation Failed!" , 400
+        self.port = port
 
-    @app.after_request
-    def identify_server(response):
-        response.headers.set(
-            "server", "battlesnake/github/starter-snake-python"
-        )
-        return response
+        self.setup_routes()
 
-    host = "0.0.0.0"
+    def setup_routes(self):
 
-    logging.getLogger("werkzeug").setLevel(logging.ERROR)
+        @self.app.get("/")
+        def on_info():
+            return self.handlers["info"]()
 
-    print(f"\nRunning Battlesnake at http://{host}:{port}")
-    app.run(host=host, port=port)
+        @self.app.post("/start")
+        def on_start():
+            game_state = request.get_json()
+            if validate_game_state(game_state):
+                self.handlers["start"](game_state)
+                return "ok"
+            else:
+                return "Game State Validation Failed!" , 400
+
+        @self.app.post("/move")
+        def on_move():
+            game_state = request.get_json()
+            if validate_game_state(game_state):
+                return self.handlers["move"](game_state)
+            else:
+                return "Game State Validation Failed!" , 400
+
+        @self.app.post("/end")
+        def on_end():
+            game_state = request.get_json()
+            if validate_game_state(game_state):
+                self.handlers["end"](game_state)
+                return "ok"
+            else:
+                return "Game State Validation Failed!" , 400
+            
+        @self.app.after_request
+        def identify_server(response):
+            response.headers.set(
+                "server", "battlesnake/github/starter-snake-python"
+            )
+            return response
+        
+    def run_server(self):
+
+        host = "0.0.0.0"
+
+        logging.getLogger("werkzeug").setLevel(logging.ERROR)
+
+        print(f"\nRunning Battlesnake at http://{host}:{self.port}")
+        self.app.run(host=host, port=self.port)
--- tests/test_bad_request.py
+++ tests/test_bad_request.py
@@ -5,6 +5,8 @@ from unittest.mock import patch
 
 from requests import request
 
+from move import Move
+
 class TestBadRequest(unittest.main):
 
     def not_bad_request(self):
@@ -14,3 +16,9 @@ class TestBadRequest(unittest.main):
         url = " http://0.0.0.0:8000"
         
         headers = {"contents": "application/json"}
+
+        with patch.object(Move, "choose_move", return_value="Called") as choose_move:
+        
+            response = request.post(data=data, url=url, headers=headers)
+
+            choose_move.assert_called_once()
