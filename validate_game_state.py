899dd07fbac36227cc68f5188a49553d8b7d0805
SuperMeister808 <emil.chatzistamatiou@gmail.com>
2026-01-13 13:53:15 +0100

    Test passed!

--- validate_game_state.py
+++ validate_game_state.py
@@ -4,17 +4,21 @@ def validate_game_state(game_state):
         important_keys = ["game", "ruleset", "squad", "turn", "board", "you"]
 
         for key , data in game_state.items():
-
-            if key != important_keys:
+            
+            if key not in important_keys:
 
                 return False
             
             if key != "turn":
+                
                 if not isinstance(data, dict):
+
                     return False
                 
             if key == "turn":
+                
                 if not isinstance(data, int):
+
                     return False
-            
+        
         return True
