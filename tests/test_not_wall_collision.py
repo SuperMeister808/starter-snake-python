44c54a2eb1644d5adcb41e0d172f3f60d497c356
SuperMeister808 <emil.chatzistamatiou@gmail.com>
2026-01-25 13:33:27 +0100

    setup patchers in test_not_wall_collision

--- tests/test_not_wall_collision.py
+++ tests/test_not_wall_collision.py
@@ -5,6 +5,39 @@ from move import Move
 
 class TestNotWallCollision(unittest.TestCase):
 
+    def setUp(self):
+        
+        self.bot = Move()
+
+        self.patchers = [
+             patch.object(self.bot, "reset_is_move_safe"),
+             patch.object(self.bot, "not_enemy_collision"),
+             patch.object(self.bot, "not_backward"),
+             patch.object(self.bot, "not_itself_collision"),
+             patch.object(self.bot, "calculate_food")      
+        ]
+
+        self.mocks = {}
+
+        for patcher in self.patchers:
+             
+            mock = patcher.start()
+            self.mocks[mock._mock_name] = mock
+
+        self.addCleanup(self.stop_patches)
+
+    def stop_patches(self):
+         
+        for patcher in self.patchers:
+
+            patcher.stop()
+
+    def check_calls(self):
+         
+        for name, mock in self.mocks.items():
+             
+            mock.assert_called_once()
+    
     def check_safe_moves(self, is_move_safe, excluded_move):
         
         for move , data in is_move_safe.items():
@@ -19,183 +52,135 @@ class TestNotWallCollision(unittest.TestCase):
     
     def test_not_left(self):
 
-        game_state = {"you": {"id": "my", "head": {"x": 0, "y": 2} ,"body": [{"x": 0, "y": 2}, {"x": 0, "y": 3}, {"x": 0, "y": 4}],"length": 3}, 
+                    game_state = {"you": {"id": "my", "head": {"x": 0, "y": 2} ,"body": [{"x": 0, "y": 2}, {"x": 0, "y": 3}, {"x": 0, "y": 4}],"length": 3}, 
                       "board": {"height": 11, "width": 11, "snakes": [], "food": [{"x": 10, "y": 10}]},
                       "turn": 1}
         
-        with patch.object(Move, "not_backward", return_value="Mocked") as not_backward:
 
-            with patch.object(Move, "not_itself_collision", return_value="Mocked") as not_itself:
-
-                with patch.object(Move, "not_enemy_collision", return_value="Mocked") as not_enemy:
                     
-                    bot = Move()
+                
 
-                    bot.choose_move(game_state)
+                    self.bot.choose_move(game_state)
                     
-                    not_backward.assert_called_once()
-
-                    not_itself.assert_called_once()
-
-                    not_enemy.assert_called_once()
+                    self.check_calls()
                     
-                    is_safe_left = bot.is_move_safe["left"]["is_safe"]
+                    is_safe_left = self.bot.is_move_safe["left"]["is_safe"]
                     
                     self.assertFalse(is_safe_left)
 
-                    self.assertTrue(self.check_safe_moves(bot.is_move_safe, "left"))
+                    self.assertTrue(self.check_safe_moves(self.bot.is_move_safe, "left"))
 
     def test_not_right(self):
 
-        game_state = {"you": {"id": "my", "head": {"x": 10, "y": 2} ,"body": [{"x": 10, "y": 2}, {"x": 10, "y": 3}, {"x": 10, "y": 4}],"length": 3}, 
+                    game_state = {"you": {"id": "my", "head": {"x": 10, "y": 2} ,"body": [{"x": 10, "y": 2}, {"x": 10, "y": 3}, {"x": 10, "y": 4}],"length": 3}, 
                       "board": {"height": 11, "width": 11, "snakes": [], "food": [{"x": 10, "y": 10}]},
                       "turn": 1}
         
-        with patch.object(Move, "not_backward", return_value="Mocked") as not_backward:
 
-            with patch.object(Move, "not_itself_collision", return_value="Mocked") as not_itself:
-
-                with patch.object(Move, "not_enemy_collision", return_value="Mocked") as not_enemy:
                     
-                    bot = Move()
+               
 
-                    bot.choose_move(game_state)
+                    self.bot.choose_move(game_state)
                     
-                    not_backward.assert_called_once()
-
-                    not_itself.assert_called_once()
-
-                    not_enemy.assert_called_once()
+                    self.check_calls()
                     
-                    is_safe_right = bot.is_move_safe["right"]["is_safe"]
+                    is_safe_right = self.bot.is_move_safe["right"]["is_safe"]
                     
                     self.assertFalse(is_safe_right)
 
-                    self.assertTrue(self.check_safe_moves(bot.is_move_safe, "right"))
+                    self.assertTrue(self.check_safe_moves(self.bot.is_move_safe, "right"))
     
     def test_not_up(self):
 
-        game_state = {"you": {"id": "my", "head": {"x": 2, "y": 10} ,"body": [{"x": 2, "y": 10}, {"x": 2, "y": 9}, {"x": 2, "y": 8}],"length": 3}, 
+                    game_state = {"you": {"id": "my", "head": {"x": 2, "y": 10} ,"body": [{"x": 2, "y": 10}, {"x": 2, "y": 9}, {"x": 2, "y": 8}],"length": 3}, 
                       "board": {"height": 11, "width": 11, "snakes": [], "food": [{"x": 10, "y": 10}]},
                       "turn": 1}
         
-        with patch.object(Move, "not_backward", return_value="Mocked") as not_backward:
 
-            with patch.object(Move, "not_itself_collision", return_value="Mocked") as not_itself:
 
-                with patch.object(Move, "not_enemy_collision", return_value="Mocked") as not_enemy:
+             
 
-                    bot = Move()
-
-                    bot.choose_move(game_state)
+                    self.bot.choose_move(game_state)
                     
-                    not_backward.assert_called_once()
-
-                    not_itself.assert_called_once()
-
-                    not_enemy.assert_called_once()
+                    self.check_calls()
                     
-                    is_safe_up = bot.is_move_safe["up"]["is_safe"]
+                    is_safe_up = self.bot.is_move_safe["up"]["is_safe"]
 
                     self.assertFalse(is_safe_up)
 
-                    self.assertTrue(self.check_safe_moves(bot.is_move_safe, "up"))
+                    self.assertTrue(self.check_safe_moves(self.bot.is_move_safe, "up"))
 
     def test_not_down(self):
 
-        game_state = {"you": {"id": "my", "head": {"x": 2, "y": 0} ,"body": [{"x": 2, "y": 0}, {"x": 3, "y": 0}, {"x": 4, "y": 0}],"length": 3}, 
+                    game_state = {"you": {"id": "my", "head": {"x": 2, "y": 0} ,"body": [{"x": 2, "y": 0}, {"x": 3, "y": 0}, {"x": 4, "y": 0}],"length": 3}, 
                       "board": {"height": 11, "width": 11, "snakes": [], "food": [{"x": 10, "y": 10}]},
                       "turn": 1}
         
-        with patch.object(Move, "not_backward", return_value="Mocked") as not_backward:
 
-            with patch.object(Move, "not_itself_collision", return_value="Mocked") as not_itself:
 
-                with patch.object(Move, "not_enemy_collision", return_value="Mocked") as not_enemy:
+          
 
-                    bot = Move()
-
-                    bot.choose_move(game_state)
+                    self.bot.choose_move(game_state)
                     
-                    not_backward.assert_called_once()
-
-                    not_itself.assert_called_once()
-
-                    not_enemy.assert_called_once()
+                    self.check_calls()
                     
-                    is_safe_down = bot.is_move_safe["down"]["is_safe"]
+                    is_safe_down = self.bot.is_move_safe["down"]["is_safe"]
 
                     self.assertFalse(is_safe_down)
 
-                    self.assertTrue(self.check_safe_moves(bot.is_move_safe, "down"))
+                    self.assertTrue(self.check_safe_moves(self.bot.is_move_safe, "down"))
     
     def test_not_up_and_right(self):
 
-        game_state = {"you": {"id": "my", "head": {"x": 10, "y": 10} ,"body": [{"x": 10, "y": 10}, {"x": 9, "y": 10}, {"x": 8, "y": 10}],"length": 3}, 
+                    game_state = {"you": {"id": "my", "head": {"x": 10, "y": 10} ,"body": [{"x": 10, "y": 10}, {"x": 9, "y": 10}, {"x": 8, "y": 10}],"length": 3}, 
                       "board": {"height": 11, "width": 11, "snakes": [], "food": [{"x": 10, "y": 10}]},
                       "turn": 1}
         
-        with patch.object(Move, "not_backward", return_value="Mocked") as not_backward:
 
-            with patch.object(Move, "not_itself_collision", return_value="Mocked") as not_itself:
 
-                with patch.object(Move, "not_enemy_collision", return_value="Mocked") as not_enemy:
+       
 
-                    bot = Move()
-
-                    bot.choose_move(game_state)
+                    self.bot.choose_move(game_state)
                     
-                    not_backward.assert_called_once()
-
-                    not_itself.assert_called_once()
-
-                    not_enemy.assert_called_once()
+                    self.check_calls()
                     
-                    is_safe_up = bot.is_move_safe["up"]["is_safe"]
+                    is_safe_up = self.bot.is_move_safe["up"]["is_safe"]
 
-                    is_safe_right = bot.is_move_safe["right"]["is_safe"]
+                    is_safe_right = self.bot.is_move_safe["right"]["is_safe"]
 
                     self.assertFalse(is_safe_up)
 
                     self.assertFalse(is_safe_right)
 
-                    self.assertTrue(bot.is_move_safe["down"]["is_safe"])
+                    self.assertTrue(self.bot.is_move_safe["down"]["is_safe"])
 
-                    self.assertTrue(bot.is_move_safe["left"]["is_safe"])
+                    self.assertTrue(self.bot.is_move_safe["left"]["is_safe"])
 
     def test_not_down_and_left(self):
 
-        game_state = {"you": {"id": "my", "head": {"x": 0, "y": 0} ,"body": [{"x": 1, "y": 0}, {"x": 2, "y": 0}, {"x": 3, "y": 0}],"length": 3}, 
+                    game_state = {"you": {"id": "my", "head": {"x": 0, "y": 0} ,"body": [{"x": 1, "y": 0}, {"x": 2, "y": 0}, {"x": 3, "y": 0}],"length": 3}, 
                       "board": {"height": 11, "width": 11, "snakes": [], "food": [{"x": 10, "y": 10}]},
                       "turn": 1}
         
-        with patch.object(Move, "not_backward", return_value="Mocked") as not_backward:
 
-            with patch.object(Move, "not_itself_collision", return_value="Mocked") as not_itself:
 
-                with patch.object(Move, "not_enemy_collision", return_value="Mocked") as not_enemy:
+            
 
-                    bot = Move()
-
-                    bot.choose_move(game_state)
+                    self.bot.choose_move(game_state)
                     
-                    not_backward.assert_called_once()
-
-                    not_itself.assert_called_once()
-
-                    not_enemy.assert_called_once()
+                    self.check_calls()
                     
-                    is_safe_down = bot.is_move_safe["down"]["is_safe"]
+                    is_safe_down = self.bot.is_move_safe["down"]["is_safe"]
 
-                    is_safe_left = bot.is_move_safe["left"]["is_safe"]
+                    is_safe_left = self.bot.is_move_safe["left"]["is_safe"]
 
                     self.assertFalse(is_safe_down)
 
                     self.assertFalse(is_safe_left)
 
-                    self.assertTrue(bot.is_move_safe["up"]["is_safe"])
+                    self.assertTrue(self.bot.is_move_safe["up"]["is_safe"])
 
-                    self.assertTrue(bot.is_move_safe["right"]["is_safe"])
+                    self.assertTrue(self.bot.is_move_safe["right"]["is_safe"])
 
 if __name__ == "__main__":
 
