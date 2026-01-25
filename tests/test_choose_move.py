f92dd92dab90beb7bf463c66a7848f425a07230e
SuperMeister808 <emil.chatzistamatiou@gmail.com>
2026-01-25 13:09:57 +0100

    setup patchers in test_choose_move

--- tests/test_choose_move.py
+++ tests/test_choose_move.py
@@ -7,323 +7,216 @@ from move import Move
 
 class TestChooseMove(unittest.TestCase):
 
+    def setUp(self):
+        
+        self.bot = Move()
+
+        self.patchers = [
+             patch.object(self.bot, "reset_is_move_safe"),
+             patch.object(self.bot, "not_enemy_collision"),
+             patch.object(self.bot, "not_backward"),
+             patch.object(self.bot, "not_itself_collision"),
+             patch.object(self.bot, "not_wall_collision"),
+             patch.object(self.bot, "calculate_food")      
+        ]
+
+        self.mocks = {}
+
+        for patcher in self.patchers:
+             
+            mock = patcher.start()
+            self.mocks[mock._mock_name] = mock
+
+        self.addCleanup(self.stop_patches)
+
+    def stop_patches(self):
+         
+        for patcher in self.patchers:
+
+            patcher.stop()
+
+    def check_calls(self):
+         
+        for name, mock in self.mocks.items():
+             
+            mock.assert_called_once()
+    
     def test_priority_left(self):
 
-        with patch.object(Move, "reset_is_move_safe") as reset, \
-             patch.object(Move, "not_backward") as not_backward, \
-             patch.object(Move, "not_wall_collision") as not_wall, \
-             patch.object(Move, "not_itself_collision") as not_itself, \
-             patch.object(Move, "not_enemy_collision") as not_enemy:
   
                 game_state = "Not_important"
 
-                bot = Move()
+
                                 
-                with patch.object(bot, "is_move_safe", {"left": {"is_safe": True, "priority": 1}, "right": {"is_safe": True, "priority": 0}, "up": {"is_safe": True, "priority": 0}, "down": {"is_safe": True, "priority": 0}}) as is_move_safe:
+                with patch.object(self.bot, "is_move_safe", {"left": {"is_safe": True, "priority": 1}, "right": {"is_safe": True, "priority": 0}, "up": {"is_safe": True, "priority": 0}, "down": {"is_safe": True, "priority": 0}}) as is_move_safe:
                                 
-                    result = bot.choose_move(game_state)
+                    result = self.bot.choose_move(game_state)
 
                     expected = ["left"]
                             
-                    reset.assert_called_once()
-
-                    not_backward.assert_called_once()
-                                
-                    not_wall.assert_called_once()
-
-                    not_itself.assert_called_once()
-
-                    not_enemy.assert_called_once()
-
-                    reset.assert_called_once()
+                    self.check_calls()
                             
                     self.assertIn(result["move"], expected)
 
     def test_priority_right(self):
 
-        with patch.object(Move, "reset_is_move_safe") as reset, \
-             patch.object(Move, "not_backward") as not_backward, \
-             patch.object(Move, "not_wall_collision") as not_wall, \
-             patch.object(Move, "not_itself_collision") as not_itself, \
-             patch.object(Move, "not_enemy_collision") as not_enemy:
+
   
                 game_state = "Not_important"
 
-                bot = Move()
+
                                 
-                with patch.object(bot, "is_move_safe", {"left": {"is_safe": True, "priority": 0}, "right": {"is_safe": True, "priority": 1}, "up": {"is_safe": True, "priority": 0}, "down": {"is_safe": True, "priority": 0}}) as is_move_safe:
+                with patch.object(self.bot, "is_move_safe", {"left": {"is_safe": True, "priority": 0}, "right": {"is_safe": True, "priority": 1}, "up": {"is_safe": True, "priority": 0}, "down": {"is_safe": True, "priority": 0}}) as is_move_safe:
                                 
-                    result = bot.choose_move(game_state)
+                    result = self.bot.choose_move(game_state)
 
                     expected = ["right"]
                             
-                    reset.assert_called_once()
-
-                    not_backward.assert_called_once()
-                                
-                    not_wall.assert_called_once()
-
-                    not_itself.assert_called_once()
-
-                    not_enemy.assert_called_once()
-
-                    reset.assert_called_once()
+                    self.check_calls()
                             
                     self.assertIn(result["move"], expected)
 
     def test_priority_up(self):
 
-        with patch.object(Move, "reset_is_move_safe") as reset, \
-             patch.object(Move, "not_backward") as not_backward, \
-             patch.object(Move, "not_wall_collision") as not_wall, \
-             patch.object(Move, "not_itself_collision") as not_itself, \
-             patch.object(Move, "not_enemy_collision") as not_enemy:
+
   
                 game_state = "Not_important"
 
-                bot = Move()
+
                 
-                with patch.object(bot, "is_move_safe", {"left": {"is_safe": True, "priority": 1}, "right": {"is_safe": True, "priority": 0}, "up": {"is_safe": True, "priority": 2}, "down": {"is_safe": True, "priority": 0}}) as is_move_safe:
+                with patch.object(self.bot, "is_move_safe", {"left": {"is_safe": True, "priority": 1}, "right": {"is_safe": True, "priority": 0}, "up": {"is_safe": True, "priority": 2}, "down": {"is_safe": True, "priority": 0}}) as is_move_safe:
                                           
-                    result = bot.choose_move(game_state)
+                    result = self.bot.choose_move(game_state)
 
                     expected = ["up"]
                             
-                    reset.assert_called_once()
-
-                    not_backward.assert_called_once()
-                                
-                    not_wall.assert_called_once()
-
-                    not_itself.assert_called_once()
-
-                    not_enemy.assert_called_once()
-
-                    reset.assert_called_once()
+                    self.check_calls()
                             
                     self.assertIn(result["move"], expected)
 
     def test_priority_down(self):
 
-        with patch.object(Move, "reset_is_move_safe") as reset, \
-             patch.object(Move, "not_backward") as not_backward, \
-             patch.object(Move, "not_wall_collision") as not_wall, \
-             patch.object(Move, "not_itself_collision") as not_itself, \
-             patch.object(Move, "not_enemy_collision") as not_enemy:
+
   
                 game_state = "Not_important"
 
-                bot = Move()
+
                                 
-                with patch.object(bot, "is_move_safe", {"left": {"is_safe": True, "priority": 0}, "right": {"is_safe": True, "priority": 1}, "up": {"is_safe": True, "priority": 0}, "down": {"is_safe": True, "priority": 2}}) as is_move_safe:
+                with patch.object(self.bot, "is_move_safe", {"left": {"is_safe": True, "priority": 0}, "right": {"is_safe": True, "priority": 1}, "up": {"is_safe": True, "priority": 0}, "down": {"is_safe": True, "priority": 2}}) as is_move_safe:
                                 
-                    result = bot.choose_move(game_state)
+                    result = self.bot.choose_move(game_state)
 
                     expected = ["down"]
                             
-                    reset.assert_called_once()
-
-                    not_backward.assert_called_once()
-                                
-                    not_wall.assert_called_once()
-
-                    not_itself.assert_called_once()
-
-                    not_enemy.assert_called_once()
-
-                    reset.assert_called_once()
+                    self.check_calls()
                             
                     self.assertIn(result["move"], expected)
 
     def test_priority_right_and_left(self):
 
-       with patch.object(Move, "reset_is_move_safe") as reset, \
-            patch.object(Move, "not_backward") as not_backward, \
-            patch.object(Move, "not_wall_collision") as not_wall, \
-            patch.object(Move, "not_itself_collision") as not_itself, \
-            patch.object(Move, "not_enemy_collision") as not_enemy:
+
   
                 game_state = "Not_important"
 
-                bot = Move()
+
                                 
-                with patch.object(bot, "is_move_safe", {"left": {"is_safe": True, "priority": 1}, "right": {"is_safe": True, "priority": 1}, "up": {"is_safe": True, "priority": 0}, "down": {"is_safe": True, "priority": 0}}) as is_move_safe:
+                with patch.object(self.bot, "is_move_safe", {"left": {"is_safe": True, "priority": 1}, "right": {"is_safe": True, "priority": 1}, "up": {"is_safe": True, "priority": 0}, "down": {"is_safe": True, "priority": 0}}) as is_move_safe:
                                 
-                    result = bot.choose_move(game_state)
+                    result = self.bot.choose_move(game_state)
 
                     expected = ["left", "right"]
                             
-                    reset.assert_called_once()
-
-                    not_backward.assert_called_once()
-                                
-                    not_wall.assert_called_once()
-
-                    not_itself.assert_called_once()
-
-                    not_enemy.assert_called_once()
-
-                    reset.assert_called_once()
+                    self.check_calls()
                             
                     self.assertIn(result["move"], expected)
 
     def test_priority_up_and_down(self):
            
-        with patch.object(Move, "reset_is_move_safe") as reset, \
-             patch.object(Move, "not_backward") as not_backward, \
-             patch.object(Move, "not_wall_collision") as not_wall, \
-             patch.object(Move, "not_itself_collision") as not_itself, \
-             patch.object(Move, "not_enemy_collision") as not_enemy:
+
       
                 game_state = "Not_important"
 
-                bot = Move()
-                                
-                with patch.object(bot, "is_move_safe", {"left": {"is_safe": True, "priority": 1}, "right": {"is_safe": True, "priority": 1}, "up": {"is_safe": True, "priority": 2}, "down": {"is_safe": True, "priority": 2}}) as is_move_safe:
 
-                    result = bot.choose_move(game_state)
+                                
+                with patch.object(self.bot, "is_move_safe", {"left": {"is_safe": True, "priority": 1}, "right": {"is_safe": True, "priority": 1}, "up": {"is_safe": True, "priority": 2}, "down": {"is_safe": True, "priority": 2}}) as is_move_safe:
+
+                    result = self.bot.choose_move(game_state)
 
                     expected = ["up", "down"]
                             
-                    reset.assert_called_once()
-
-                    not_backward.assert_called_once()
-                                
-                    not_wall.assert_called_once()
-
-                    not_itself.assert_called_once()
-
-                    not_enemy.assert_called_once()
-
-                    reset.assert_called_once()
+                    self.check_calls()
                             
                     self.assertIn(result["move"], expected)
 
     def test_unsafe_move(self):
          
-        with patch.object(Move, "reset_is_move_safe") as reset, \
-             patch.object(Move, "not_backward") as not_backward, \
-             patch.object(Move, "not_wall_collision") as not_wall, \
-             patch.object(Move, "not_itself_collision") as not_itself, \
-             patch.object(Move, "not_enemy_collision") as not_enemy:
+
                
                 game_state = "Not important"
 
-                bot = Move()
 
-                with patch.object(bot, "is_move_safe", {"left": {"is_safe": False, "priority": 0}, "right": {"is_safe": True, "priority": 0}, "up": {"is_safe": True, "priority": 0}, "down": {"is_safe": True, "priority": 0}}) as is_move_safe:
 
-                    result = bot.choose_move(game_state)
+                with patch.object(self.bot, "is_move_safe", {"left": {"is_safe": False, "priority": 0}, "right": {"is_safe": True, "priority": 0}, "up": {"is_safe": True, "priority": 0}, "down": {"is_safe": True, "priority": 0}}) as is_move_safe:
+
+                    result = self.bot.choose_move(game_state)
 
                     expected = ["up", "down", "right"]
                             
-                    reset.assert_called_once()
-
-                    not_backward.assert_called_once()
-                                
-                    not_wall.assert_called_once()
-
-                    not_itself.assert_called_once()
-
-                    not_enemy.assert_called_once()
-
-                    reset.assert_called_once()
+                    self.check_calls()
                             
                     self.assertIn(result["move"], expected)
 
     def test_priority_and_unsafe(self):
 
-        with patch.object(Move, "reset_is_move_safe") as reset, \
-             patch.object(Move, "not_backward") as not_backward, \
-             patch.object(Move, "not_wall_collision") as not_wall, \
-             patch.object(Move, "not_itself_collision") as not_itself, \
-             patch.object(Move, "not_enemy_collision") as not_enemy:
+
                
                 game_state = "Not important"
 
-                bot = Move()
+
                                 
-                with patch.object(bot, "is_move_safe", {"left": {"is_safe": True, "priority": 1}, "right": {"is_safe": False, "priority": 0}, "up": {"is_safe": False, "priority": 0}, "down": {"is_safe": True, "priority": 2}}) as is_move_safe:
+                with patch.object(self.bot, "is_move_safe", {"left": {"is_safe": True, "priority": 1}, "right": {"is_safe": False, "priority": 0}, "up": {"is_safe": False, "priority": 0}, "down": {"is_safe": True, "priority": 2}}) as is_move_safe:
                 
-                    result = bot.choose_move(game_state)
+                    result = self.bot.choose_move(game_state)
 
                     expected = ["down"]
                             
-                    reset.assert_called_once()
-
-                    not_backward.assert_called_once()
-                                
-                    not_wall.assert_called_once()
-
-                    not_itself.assert_called_once()
-
-                    not_enemy.assert_called_once()
-
-                    reset.assert_called_once()
+                    self.check_calls()
                             
                     self.assertIn(result["move"], expected)
 
     def test_ever_move_unsafe(self):
                             
-        with patch.object(Move, "reset_is_move_safe") as reset, \
-             patch.object(Move, "not_backward") as not_backward, \
-             patch.object(Move, "not_wall_collision") as not_wall, \
-             patch.object(Move, "not_itself_collision") as not_itself, \
-             patch.object(Move, "not_enemy_collision") as not_enemy:
+
   
                 game_state = {"turn": 1}
 
-                bot = Move()
+
                                 
-                with patch.object(bot, "is_move_safe", {"left": {"is_safe": False, "priority": 0}, "right": {"is_safe": False, "priority": 0}, "up": {"is_safe": False, "priority": 0}, "down": {"is_safe": False, "priority": 0}}) as is_move_safe:
+                with patch.object(self.bot, "is_move_safe", {"left": {"is_safe": False, "priority": 0}, "right": {"is_safe": False, "priority": 0}, "up": {"is_safe": False, "priority": 0}, "down": {"is_safe": False, "priority": 0}}) as is_move_safe:
                                 
-                    result = bot.choose_move(game_state)
+                    result = self.bot.choose_move(game_state)
 
                     expected = ["down"]
                             
-                    reset.assert_called_once()
-
-                    not_backward.assert_called_once()
-                                
-                    not_wall.assert_called_once()
-
-                    not_itself.assert_called_once()
-
-                    not_enemy.assert_called_once()
-
-                    reset.assert_called_once()
+                    self.check_calls()
                             
                     self.assertIn(result["move"], expected)
 
     def test_multiple_priorities(self):
          
-         with patch.object(Move, "reset_is_move_safe") as reset, \
-             patch.object(Move, "not_backward") as not_backward, \
-             patch.object(Move, "not_wall_collision") as not_wall, \
-             patch.object(Move, "not_itself_collision") as not_itself, \
-             patch.object(Move, "not_enemy_collision") as not_enemy:
+
   
                 game_state = {"turn": 1}
 
-                bot = Move()
+
                                 
-                with patch.object(bot, "is_move_safe", {"left": {"is_safe": True, "priority": 1}, "right": {"is_safe": True, "priority": 2}, "up": {"is_safe": True, "priority": 1}, "down": {"is_safe": True, "priority": 2}}) as is_move_safe:
+                with patch.object(self.bot, "is_move_safe", {"left": {"is_safe": True, "priority": 1}, "right": {"is_safe": True, "priority": 2}, "up": {"is_safe": True, "priority": 1}, "down": {"is_safe": True, "priority": 2}}) as is_move_safe:
                                 
-                    result = bot.choose_move(game_state)
+                    result = self.bot.choose_move(game_state)
 
                     expected = ["down", "right"]
                             
-                    reset.assert_called_once()
-
-                    not_backward.assert_called_once()
-                                
-                    not_wall.assert_called_once()
-
-                    not_itself.assert_called_once()
-
-                    not_enemy.assert_called_once()
-
-                    reset.assert_called_once()
+                    self.check_calls()
                             
                     self.assertIn(result["move"], expected)
 
