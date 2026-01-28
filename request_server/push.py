ae149c1e637a04ac17d5d8e9e74298b9111985b0
SuperMeister808 <emil.chatzistamatiou@gmail.com>
2026-01-28 17:13:27 +0100

    changed request

--- request_server/push.py
+++ request_server/push.py
@@ -1,6 +1,8 @@
 
 import requests
 
-response = requests.get("http://192.168.2.116:8000/admin/push")
+
+headers = {"X-ADMIN-TOKEN": "1123581321"}
+response = requests.get(url="http://192.168.2.116:8000/admin/push", headers=headers)
 
 print(response.json())
