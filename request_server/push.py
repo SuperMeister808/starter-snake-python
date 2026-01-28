11c1719529d37b5106daaa444f790b79872e897d
SuperMeister808 <emil.chatzistamatiou@gmail.com>
2026-01-28 17:50:23 +0100

    bug-fix: name error headers

--- request_server/push.py
+++ request_server/push.py
@@ -2,7 +2,7 @@
 import requests
 
 
-headers = {"X-ADMIN-TOKEN": "1123581321"}
+headers = {"X-Admin-Token": "1123581321"}
 response = requests.get(url="http://192.168.2.116:8000/admin/push", headers=headers)
 
 print(response.json())
