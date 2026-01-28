edd5e300e39e389b8b02bd4b9fda8b1fcf13e3e2
SuperMeister808 <emil.chatzistamatiou@gmail.com>
2026-01-28 18:32:19 +0100

    bug-fix: wrong requests method in incorrect_push

--- request_server/incorrect_push.py
+++ request_server/incorrect_push.py
@@ -2,6 +2,6 @@
 import requests
 
 headers = {"X-Admin-Token": "12345678"}
-response = requests.post(url="http://192.168.2.116:8000/admin/push", headers=headers)
+response = requests.get(url="http://192.168.2.116:8000/admin/push", headers=headers)
 
 print(response.json())
