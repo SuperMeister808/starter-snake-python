a6f1b1dea1e75b59ce9785983b611799a7a176f2
SuperMeister808 <emil.chatzistamatiou@gmail.com>
2026-02-01 18:05:49 +0100

    test_written_data added to test_clear_emergency_logger

--- tests/emergency_logger/test_clear_emergency_logger.py
+++ tests/emergency_logger/test_clear_emergency_logger.py
@@ -3,23 +3,37 @@ import unittest
 from unittest.mock import patch
 
 from emergency_logger import EmergencyLogger
+from print_collector import PrintCollector
 
 from queue import Queue
 
 class TestClearEmergencyLogger(unittest.TestCase):
 
     
-    def test_clear_data(self):
+    def test_written_data(self):
 
         q = Queue()
         q.put("where", "exception", "game_state")
         
+        print_collector = PrintCollector()
+        print_collector.collect_message("text1")
+
         with (
             patch.object(EmergencyLogger, "loger_queue", new=q),
-
+            patch.object(EmergencyLogger, "flags", new={"testing...": "Testing..."}),
+            patch.object(EmergencyLogger, "print_collector", new=print_collector)
         ):
-        
+            EmergencyLogger.clear_emergency_logger()
+
+            self.assertTrue(EmergencyLogger.loger_queue.empty())
+            self.assertEqual(EmergencyLogger.flags, {"is_running": False, "worker_thread": None})
+            messages = EmergencyLogger.print_collector.messages
+            self.assertEqual(len(messages), 0)
 
     def test_already_cleared(self):
 
         pass
+
+if __name__ == "__main__":
+
+    unittest.main()
