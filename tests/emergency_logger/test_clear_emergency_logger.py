2737fe04c3bd53a3ee715e613efe428c5b9ae744
SuperMeister808 <emil.chatzistamatiou@gmail.com>
2026-02-01 18:08:11 +0100

    test_already_cleared added to test_clear_emergency_logger

--- tests/emergency_logger/test_clear_emergency_logger.py
+++ tests/emergency_logger/test_clear_emergency_logger.py
@@ -32,7 +32,21 @@ class TestClearEmergencyLogger(unittest.TestCase):
 
     def test_already_cleared(self):
 
-        pass
+        q = Queue()
+        
+        print_collector = PrintCollector()
+
+        with (
+            patch.object(EmergencyLogger, "loger_queue", new=q),
+            patch.object(EmergencyLogger, "flags", new={"is_running": False, "worker_thread": None}),
+            patch.object(EmergencyLogger, "print_collector", new=print_collector)
+        ):
+            EmergencyLogger.clear_emergency_logger()
+
+            self.assertTrue(EmergencyLogger.loger_queue.empty())
+            self.assertEqual(EmergencyLogger.flags, {"is_running": False, "worker_thread": None})
+            messages = EmergencyLogger.print_collector.messages
+            self.assertEqual(len(messages), 0)
 
 if __name__ == "__main__":
 
