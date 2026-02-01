db96bb3cacd7470ddfba50941824c030ea3fd05f
SuperMeister808 <emil.chatzistamatiou@gmail.com>
2026-02-01 18:17:22 +0100

    setup test_upload_to_git

---
+++ tests/emergency_logger/test_upload_to_git.py
@@ -0,0 +1,13 @@
+
+import unittest
+from unittest.mock import patch
+
+class TestUploadToGit(unittest.TestCase):
+
+    def test_correct_branch(self):
+
+        pass
+
+    def test_wrong_branch(self):
+
+        pass
