e46f52f9a17392974a0e9e46ae5d6934f8b657eb
SuperMeister808 <emil.chatzistamatiou@gmail.com>
2026-02-03 18:47:20 +0100

    setup test_random_choice

--- tests/emergency_code/test_random_choice.py
+++ tests/emergency_code/test_random_choice.py
@@ -2,8 +2,36 @@
 import unittest
 from unittest.mock import patch , MagicMock
 
+from move import Move
+
 class TestRandomChoice(unittest.TestCase):
 
+    def setUp(self):
+        
+        self.bot = Move()
+
+        self.patchers = [
+            patch("move.random.choice")
+        ]
+
+        mocks = {}
+
+        for patcher in self.patchers:
+
+            mock = patcher.start()
+            mocks ["mock_random"] = mock
+
+        self.mock_random = mocks ["mock_random"]
+        self.mock_random.return_value = "left"
+
+        self.addCleanup(self.stop_patchers)
+
+    def stop_patchers(self):
+
+        for patcher in self.patchers:
+
+            patcher.stop()
+    
     def test_random_choice_memory_moves(self):
 
         pass
