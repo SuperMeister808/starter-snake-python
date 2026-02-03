fd54f1bf76ba1dd60edef9288263c5b3309a3736
SuperMeister808 <emil.chatzistamatiou@gmail.com>
2026-02-03 16:54:22 +0100

    created test_get_safe_moves

---
+++ tests/emergency_code/test_get_safe_moves.py
@@ -0,0 +1,15 @@
+
+import unittest
+from unittest.mock import patch , MagicMock
+
+class TestGetSafeMoves(unittest.TestCase):
+
+    
+    
+    def test_correct_is_move_safe(self):
+
+        pass
+
+    def test_exception_is_move_safe(self):
+
+        pass
