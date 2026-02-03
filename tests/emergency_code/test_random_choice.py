07814fabd10e0a5e6c0a10e3a38400adafab14a1
SuperMeister808 <emil.chatzistamatiou@gmail.com>
2026-02-03 18:40:09 +0100

    test_random_choice created

---
+++ tests/emergency_code/test_random_choice.py
@@ -0,0 +1,21 @@
+
+import unittest
+from unittest.mock import patch , MagicMock
+
+class TestRandomChoice(unittest.TestCase):
+
+    def test_random_choice_memory_moves(self):
+
+        pass
+
+    def test_random_choice_safe_moves(self):
+
+        pass
+
+    def test_random_choice_emergency_moves(self):
+
+        pass
+
+    def test_move_down(self):
+
+        pass
