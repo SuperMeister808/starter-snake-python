9074102ab66b58839329d376bf7d7b4f7044bef8
SuperMeister808 <emil.chatzistamatiou@gmail.com>
2026-02-03 17:46:31 +0100

    test_get_priority_moves created

---
+++ tests/emergency_code/test_get_priority_moves.py
@@ -0,0 +1,17 @@
+
+import unittest
+from unittest.mock import patch , MagicMock
+
+class TestGetPriorityMoves():
+
+    def test_one_priority_move(self):
+
+        pass
+
+    def test_multiple_priority_moves(self):
+
+        pass
+
+    def test_exception(self):
+
+        pass
