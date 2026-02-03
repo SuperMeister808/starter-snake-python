d08b53ee16e127f88b63af81c84550b6ed929567
SuperMeister808 <emil.chatzistamatiou@gmail.com>
2026-02-03 14:01:11 +0100

    working stoped, need to refactor emergency code for better testing...

--- tests/emergency_code/test_choose_move.py
+++ tests/emergency_code/test_choose_move.py
@@ -1,14 +1,44 @@
 
 import unittest
-from unittest.mock import patch
+from unittest.mock import patch , MagicMock
+
+from emergency_logger import EmergencyLogger
+from move import Move
 
 class TestChooseMove(unittest.TestCase):
 
-    
-    
-    def test_reset_is_move_safe(self):
+    def setUp(self):
+        
+        self.patchers = [
+            patch.object(EmergencyLogger, "loger_queue")
+        ]
 
-        pass
+        self.mocks = {}
+        
+        for patcher in self.patchers:
+
+            mock = patcher.start()
+            self.mocks ["EmergencyLogger.loger_queue"] = mock
+
+        mocked_loger_queue = self.mocks ["EmergencyLogger.loger_queue"]
+        mocked_loger_queue.put = MagicMock()
+
+        self.addCleanup(self.stop_patchers)
+
+    def stop_patchers(self):
+
+        for patcher in self.patchers:
+
+            patcher.stop()
+    
+    @patch.object(Move, "reset_is_move_safe")
+    def test_reset_is_move_safe(self, mock_reset):
+
+        mock_reset.side_effect = RuntimeError("side effect")
+
+        bot = Move()
+
+        bot.choose_move
 
     def test_not_backward(self):
 
