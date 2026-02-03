f84ca370aed00e7e728df6bd6ce52e076b387375
SuperMeister808 <emil.chatzistamatiou@gmail.com>
2026-02-03 13:44:29 +0100

    created test_choose_move in tests/emergency_code

---
+++ tests/emergency_code/test_choose_move.py
@@ -0,0 +1,47 @@
+
+import unittest
+from unittest.mock import patch
+
+class TestChooseMove(unittest.TestCase):
+
+    
+    
+    def test_reset_is_move_safe(self):
+
+        pass
+
+    def test_not_backward(self):
+
+        pass
+
+    def test_not_wall_collision(self):
+
+        pass
+
+    def test_not_itself_collision(self):
+
+        pass
+
+    def test_not_enemy_collision(self):
+
+        pass
+
+    def test_calculate_food(self):
+
+        pass
+
+    def test_safe_moves(self):
+
+        pass
+
+    def test_priority_moves(self):
+
+        pass
+
+    def test_random_choice_priority_moves(self):
+
+        pass
+
+    def test_random_choice_safe_moves(self):
+
+        pass
