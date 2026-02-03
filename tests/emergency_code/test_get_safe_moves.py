d962ac27241b53d2f608b8205caadf29855efe65
SuperMeister808 <emil.chatzistamatiou@gmail.com>
2026-02-03 17:00:49 +0100

    setup test_get_safe_moves

--- tests/emergency_code/test_get_safe_moves.py
+++ tests/emergency_code/test_get_safe_moves.py
@@ -2,9 +2,15 @@
 import unittest
 from unittest.mock import patch , MagicMock
 
+from move import Move
+
 class TestGetSafeMoves(unittest.TestCase):
 
-    
+    def setUp(self):
+        
+        self.bot = Move()
+        
+        
     
     def test_correct_is_move_safe(self):
 
