aa95a83fb8ecdb9abcd24ba3dc1120e0092aa745
SuperMeister808 <emil.chatzistamatiou@gmail.com>
2026-02-03 18:54:42 +0100

    test_random_choice_memory_moves added to test_random_choice

--- tests/emergency_code/test_random_choice.py
+++ tests/emergency_code/test_random_choice.py
@@ -20,7 +20,20 @@ class TestRandomChoice(unittest.TestCase):
     
     def test_random_choice_memory_moves(self):
 
-        pass
+        game_state = {"testing...": "testing..."}
+
+        memory_moves = ["left", "up"]
+        safe_moves = ["left", "right", "up"]
+
+        result = self.bot.random_choice(game_state, memory_moves, safe_moves)
+
+        expected = ["left", "up"]
+        next_move = result["move"]
+
+        self.assertEqual(result, {"move": next_move})
+        self.assertIn(next_move, expected)
+        
+
 
     def test_random_choice_safe_moves(self):
 
@@ -33,3 +46,7 @@ class TestRandomChoice(unittest.TestCase):
     def test_move_down(self):
 
         pass
+
+if __name__ == "__main__":
+
+    unittest.main()
