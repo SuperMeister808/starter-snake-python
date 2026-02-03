91cdc5ffccf712feafaeefd11caafd78c438f043
SuperMeister808 <emil.chatzistamatiou@gmail.com>
2026-02-03 12:21:54 +0100

    setup mocks in test_upload_to_git

--- tests/emergency_logger/test_upload_to_git.py
+++ tests/emergency_logger/test_upload_to_git.py
@@ -6,25 +6,46 @@ from emergency_logger import EmergencyLogger
 
 class TestUploadToGit(unittest.TestCase):
 
+    def setUp(self):
+        
+
+        self.patchers = [
+            patch("emergency_logger.Repo")
+        ]
+
+        mocks = {}
+
+        for patcher in self.patchers:
+        
+            mock = patcher.start()
+
+            mocks ["mock_repo"] = mock
+
+        #man erzeugt eine Instanz des Mock Objektes, da die Klasse repo auch als Instanz aufgerufen wird
+        self.mock_repo_instance = mocks["mock_repo"].return_value
+        #man kann Attribute von mock Objekten definieren, nötig für den Test-Code
+        self.mock_repo_instance.git.add = MagicMock()
+        self.mock_repo_instance.index.commit = MagicMock()
+        self.mock_repo_instance.active_branch = MagicMock()
+
+        self.addCleanup(self.stop_patcher)
+
+    def stop_patcher(self):
+
+        for patcher in self.patchers:
+
+            patcher.stop()
+
     #testet nur Behaviour nicht Funktion
     
-    @patch("emergency_logger.Repo")
-    def test_correct_branch(self, mock_repo):
+    def test_correct_branch(self):
 
-        #man ruft den mock auf eine Instanz auf
-        mock_repo_instance = mock_repo.return_value
-        #ruft eine Instanz eines Mock Objektes auf
-        mock_repo_instance.git.add = MagicMock()
-        mock_repo_instance.index.commit = MagicMock()
-        mock_repo_instance.active_branch = MagicMock()
-        #man kann Attribute definieren, da mock Objekte ein dynamisches __dict__ besitzen
-        mock_repo_instance.active_branch.name = "test_branch"
+        self.mock_repo_instance.active_branch.name = "test_branch"
 
         EmergencyLogger.upload_to_git("/testing...", "testing...", "test_branch")
 
-        mock_repo_instance.git.add.assert_called_once_with(A=True)
-        mock_repo_instance.git.commit.assert_called_once_with("-m", "testing...", "--allow-empty")
-
+        self.mock_repo_instance.git.add.assert_called_once_with(A=True)
+        self.mock_repo_instance.git.commit.assert_called_once_with("-m", "testing...", "--allow-empty")
 
 
     def test_wrong_branch(self):
