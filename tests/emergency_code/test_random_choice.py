a4577e1c95833a299c4d2a44db6c0d900f253e6e
SuperMeister808 <emil.chatzistamatiou@gmail.com>
2026-02-03 19:00:17 +0100

    refactored setup

--- tests/emergency_code/test_random_choice.py
+++ tests/emergency_code/test_random_choice.py
@@ -3,6 +3,7 @@ import unittest
 from unittest.mock import patch , MagicMock
 
 from move import Move
+from emergency_logger import EmergencyLogger
 
 class TestRandomChoice(unittest.TestCase):
 
@@ -10,7 +11,21 @@ class TestRandomChoice(unittest.TestCase):
         
         self.bot = Move()
 
+        self.patchers = [
+            patch.object(EmergencyLogger, "loger_queue")
+        ]
 
+        mocks = {}
+
+        for patcher in self.patchers:
+
+            mock = patcher.start()
+            mocks ["mock_loger_queue"] = mock
+
+        self.mock_loger_queue = mocks ["mock_loger_queue"]
+        self.mock_loger_queue.put = MagicMock()
+        
+        self.addCleanup(self.stop_patchers)
 
     def stop_patchers(self):
 
@@ -26,14 +41,13 @@ class TestRandomChoice(unittest.TestCase):
         safe_moves = ["left", "right", "up"]
 
         result = self.bot.random_choice(game_state, memory_moves, safe_moves)
+        self.mock_loger_queue.put.assert_not_called()
 
         expected = ["left", "up"]
         next_move = result["move"]
 
         self.assertEqual(result, {"move": next_move})
         self.assertIn(next_move, expected)
-        
-
 
     def test_random_choice_safe_moves(self):
 
