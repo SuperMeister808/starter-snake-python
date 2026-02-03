849a1740c4a3ddda40a24f812001dcf9850ff9d7
SuperMeister808 <emil.chatzistamatiou@gmail.com>
2026-02-03 17:19:27 +0100

    test_correct_is_move_safe in test_get_safe_moves

--- tests/emergency_code/test_get_safe_moves.py
+++ tests/emergency_code/test_get_safe_moves.py
@@ -3,19 +3,53 @@ import unittest
 from unittest.mock import patch , MagicMock
 
 from move import Move
+from emergency_logger import EmergencyLogger
 
 class TestGetSafeMoves(unittest.TestCase):
-
+    
     def setUp(self):
         
-        self.bot = Move()
+        self.patchers = [
+            patch.object(EmergencyLogger, "loger_queue")
+        ]
+
+        mocks = {}
+
+        for patcher in self.patchers:
+
+            mock = patcher.start()
+            mocks ["mock_loger_queue"] = mock
+
+        self.mock_loger_queue = mocks ["mock_loger_queue"]
+        self.mock_loger_queue.put = MagicMock()
         
-        
+        self.addCleanup(self.stop_patcher)
+
+    def stop_patcher(self):
+
+        for patcher in self.patchers:
+
+            patcher.stop()
     
+
     def test_correct_is_move_safe(self):
 
-        pass
+        bot = Move()
+        
+        with patch.object(bot, "is_move_safe", new={"left": {"is_safe": False, "priority": 3}, "right": {"is_safe": True, "priority": 2}, "up": {"is_safe": False, "priority": 1}, "down": {"is_safe": True, "priority": 0}}):
+
+           game_state = {"testing...": "testing..."}
+           result = bot.get_safe_moves(game_state)
+
+           self.mock_loger_queue.put.assert_not_called()
+           
+           expected = {"right": 2, "down": 0}
+           self.assertEqual(result, expected)
 
     def test_exception_is_move_safe(self):
 
         pass
+
+if __name__ == "__main__":
+
+    unittest.main()
