0102ef4cfd6731bc45ebb8f7fea43dba35bc0144
SuperMeister808 <emil.chatzistamatiou@gmail.com>
2026-02-03 15:17:06 +0100

    created test_emergency_system

---
+++ tests/emergency_code/test_emergency_system.py
@@ -0,0 +1,17 @@
+
+import unittest
+from unittest.mock import patch , MagicMock
+
+class TestEmergencySystem(unittest.TestCase):
+
+    def test_correct_call(self):
+
+        pass
+
+    def test_exception_reset_is_move_safe(self):
+
+        pass
+
+    def test_exception(self):
+
+        pass
