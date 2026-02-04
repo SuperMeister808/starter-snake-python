dce106583416ebdce0d7186a99659e051b4e3227
SuperMeister808 <emil.chatzistamatiou@gmail.com>
2026-02-04 21:45:07 +0100

    changed code for safety reasons

--- request_server/push.py
+++ request_server/push.py
@@ -1,8 +1,9 @@
 
 import requests
+import os
 
 
-headers = {"X-Admin-Token": "1123581321"}
+headers = {"X-Admin-Token": os.environ.get("ADMIN_TOKEN")}
 response = requests.get(url="http://192.168.2.116:8000/admin/push", headers=headers)
 
 print(response.json())
