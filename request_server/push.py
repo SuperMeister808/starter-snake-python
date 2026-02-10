c287af8789cc6143a43e814861efb04e01f21f18
Emil Chatzistamatiou <emil.chatzistamatiou@gmail.com>
2026-02-10 14:46:12 +0100

    added correct server url

--- request_server/push.py
+++ request_server/push.py
@@ -4,6 +4,6 @@ import os
 
 
 headers = {"X-Admin-Token": os.environ.get("ADMIN_TOKEN")}
-response = requests.get(url="http://192.168.2.116:8000/admin/push", headers=headers)
+response = requests.get(url="http://192.168.2.127:8000/admin/push", headers=headers)
 
 print(response.json())
