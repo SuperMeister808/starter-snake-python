ded28b492b457e8320e2a09f38e5e824b7d8ad4d
SuperMeister808 <emil.chatzistamatiou@gmail.com>
2026-02-11 17:41:03 +0100

    setup test_get_body

---
+++ tests/test_move/test_get_body.py
@@ -0,0 +1,23 @@
+
+import unittest
+from unittest.mock import patch
+
+from move import Move
+
+class TestGetBody(unittest.TestCase):
+
+    def setUp(self):
+        
+        bot = Move()
+
+    def test_move_left(self):
+
+        pass
+
+    def test_move_down(self):
+
+        pass
+
+    def test_irregular_move(self):
+
+        pass
