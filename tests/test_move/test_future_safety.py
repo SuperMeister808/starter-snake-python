2c4683d59221cd84aa8d5721df934f90b21c9a52
Emil Chatzistamatiou <emil.chatzistamatiou@gmail.com>
2026-02-15 16:10:02 +0100

    test_one_safe_move_left added to test_future_safety

--- tests/test_move/test_future_safety.py
+++ tests/test_move/test_future_safety.py
@@ -13,19 +13,17 @@ class TestFutureSafety(unittest.TestCase):
     def setup_patchers(self):
 
         self.patchers = [
-            patch.object(self.bot, "check_moves", return_value=None)
+            patch.object(self.bot, "check_moves", return_value=None),
+            patch.object(self.bot, "reset_is_move_safe")
         ]
-        self.patcher_is_move_safe = patch.object(self.bot, "is_move_safe", new={"up": {"is_safe": True, "priority": 0}, 
-                             "down": {"is_safe": True, "priority": 0}, 
-                             "left": {"is_safe": True, "priority": 0}, 
-                             "right": {"is_safe": True, "priority": 0}})
+        
 
         self.mocks = {}
 
-        for patcher in self.patchers:
+        for i, patcher in enumerate(self.patchers):
             mock = patcher.start()
-            self.mocks[mock._name] = mock
-        self.patcher_is_move_safe.start()
+            self.mocks[i] = mock
+       
 
     def stop_patchers(self):
 
@@ -36,19 +34,51 @@ class TestFutureSafety(unittest.TestCase):
     def check_calls(self):
 
         for name , mock in self.mocks.items():
-            self.assertEqual(mock.call_count, 2)
+            self.assertTrue(mock.called)
     
-    def test_behauvior(self):
-
+    def test_safe_moves(self):
+        
         self.setup_patchers()
         
         game_state = {"you": {"body": [{"x": 1, "y": 1}, {"x": 2, "y": 1}, {"x": 3, "y": 1}], "head": {"x": 1, "y": 1}}}
         move = "left"
+        self.patcher_is_move_safe = patch.object(self.bot, "is_move_safe", new={"up": {"is_safe": True, "priority": 0}, 
+                             "down": {"is_safe": True, "priority": 0}, 
+                             "left": {"is_safe": True, "priority": 0}, 
+                             "right": {"is_safe": True, "priority": 0}})
+        self.patcher_is_move_safe.start()
 
         result = self.bot.call_future_safety(move, game_state)
 
         self.check_calls()
         self.assertEqual(result, True)
+
+        self.stop_patchers()
+
+    def test_no_safe_moves(self):
+
+        self.setup_patchers()
+        
+       
+
+    def test_one_safe_move_left(self):
+
+        self.setup_patchers()
+
+        game_state = {"you": {"body": [{"x": 1, "y": 1}, {"x": 2, "y": 1}, {"x": 3, "y": 1}], "head": {"x": 1, "y": 1}}}
+        move = "left"
+        self.patcher_is_move_safe = patch.object(self.bot, "is_move_safe", new={"up": {"is_safe": False, "priority": 0}, 
+                             "down": {"is_safe": True, "priority": 0}, 
+                             "left": {"is_safe": False, "priority": 0}, 
+                             "right": {"is_safe": False, "priority": 0}})
+        self.patcher_is_move_safe.start()
+
+        result = self.bot.call_future_safety(move, game_state)
+
+        self.check_calls()
+        self.assertEqual(result, True)
+
+        self.stop_patchers()
 
 if __name__ == "__main__":
     unittest.main()
