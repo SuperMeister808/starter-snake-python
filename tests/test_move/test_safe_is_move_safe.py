d12954cf6c0b747fdae7a25010b4c968dcd4948f
Emil Chatzistamatiou <emil.chatzistamatiou@gmail.com>
2026-02-15 20:19:18 +0100

    test_safe_is_move_safe created

---
+++ tests/test_move/test_safe_is_move_safe.py
@@ -0,0 +1,13 @@
+
+import unittest
+from unittest.mock import patch
+
+class TestSafeIsMoveSafe(unittest.TestCase):
+
+    def test_regular_dictionary(self):#
+
+        pass
+
+    def test_irregular_datatype(self):
+
+        pass
