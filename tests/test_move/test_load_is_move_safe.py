4775e5c12dbd97fd7ed0a5a6e014cc1f277b4939
Emil Chatzistamatiou <emil.chatzistamatiou@gmail.com>
2026-02-16 15:15:07 +0100

    test_irregular_datatype added to test_load_is_move_safe

--- tests/test_move/test_load_is_move_safe.py
+++ tests/test_move/test_load_is_move_safe.py
@@ -66,7 +66,24 @@ class TestLoadIsMoveSafe(unittest.TestCase):
 
     def test_irreglular_datatype(self):
 
-        pass
+        patcher_is_move_safe = ["left", "right", "up", "down"]
+        patcher_is_move_safe_memory = []
+        self.setup_patchers(patcher_is_move_safe, patcher_is_move_safe_memory)
+        
+        self.start_patchers()
+
+        self.bot.load_is_move_safe()
+
+        expected_is_move_safe = []
+        expected_is_move_safe_memory = {"up": {"is_safe": True, "priority": 0}, 
+                             "down": {"is_safe": True, "priority": 0}, 
+                             "left": {"is_safe": True, "priority": 0}, 
+                             "right": {"is_safe": True, "priority": 0}}
+        
+        self.assertEqual(self.bot.is_move_safe, expected_is_move_safe)
+        self.assertEqual(self.bot.is_move_safe_memory, expected_is_move_safe_memory)
+
+        self.stop_patchers()
 
 if __name__ == "__main__":
 
