2d5e498fdd3b143cbe03e8f0c87671595730c088
Emil Chatzistamatiou <emil.chatzistamatiou@gmail.com>
2026-02-16 15:03:09 +0100

    setup patchers

--- tests/test_move/test_load_is_move_safe.py
+++ tests/test_move/test_load_is_move_safe.py
@@ -9,14 +9,49 @@ class TestLoadIsMoveSafe(unittest.TestCase):
     def setUp(self):
         
         self.bot = Move()
-    
-    def test_possible_scenario(self):
 
-        pass
+    def setup_patchers(self, patcher_is_move_safe, patcher_is_move_safe_memory):
+
+        self.patchers = []
+
+        patcher_is_move_safe = self.create_patcher_is_move_safe(patcher_is_move_safe)
+        self.patchers.append(patcher_is_move_safe)
+
+        patcher_is_move_safe_memory = self.create_patcher_is_move_safe(patcher_is_move_safe_memory)
+        self.patchers.append(patcher_is_move_safe_memory)
+
+    def start_patchers(self):
+
+        for patcher in self.patchers:
+            patcher.start()
+
+    def stop_patchers(self):
+
+        for patcher in self.patchers:
+            patcher.stop()
+
+    def create_patcher_is_move_safe(self, patcher):
+
+        patcher = patch.object(self.bot, "is_move_safe", new=patcher)
+        return patcher
+    
+    def create_patcher_is_move_safe_memory(self, patcher):
+
+        patcher = patch.object(self.bot, "is_move_safe_memory", new=patcher)
+        return patcher
+
     
     def test_regular_datatype(self):
 
-        pass
+        patcher_is_move_safe = {"left": {}, "right": {}, "up": {}, "down": {}}
+        patcher_is_move_safe_memory = {}
+        self.setup_patchers(patcher_is_move_safe, patcher_is_move_safe_memory)
+        
+        self.start_patchers()
+        
+
+
+
 
     def test_irreglular_datatype(self):
 
