6399a4276e0058fb24b7dc9bec108d5f3123adea
Emil Chatzistamatiou <emil.chatzistamatiou@gmail.com>
2026-02-16 15:39:56 +0100

    setup test_check_moves

--- tests/test_move/test_check_moves.py
+++ tests/test_move/test_check_moves.py
@@ -10,6 +10,40 @@ class TestCheckMoves(unittest.TestCase):
         
         self.bot = Move()
 
+        self.addCleanup(self.stop_patchers)
+
+
+
+    def setup_patchers(self, patcher_emergency_system_return_value):
+
+        self.patchers = []
+
+        patcher_emergency_system = self.create_patcher_emergency_system(patcher_emergency_system_return_value)
+        self.patchers.append(patcher_emergency_system)
+
+        self.start_patchers()
+
+    def start_patchers(self):
+
+        self.mocks = {}
+
+        for i, patcher in enumerate(self.patchers):
+            mock = patcher.start()
+            self.mocks[i] = mock
+
+
+
+    def stop_patchers(self):
+
+        for patcher in self.patchers:
+            patcher.stop()
+
+    def create_patcher_emergency_system(self, patcher_emergency_system_return_value):
+
+        patcher = patch.object(self.bot, "emergency_system", return_value=patcher_emergency_system_return_value)
+        
+        return patcher
+
     def test_call_emergency_system_no_return_value(self):
 
         pass
