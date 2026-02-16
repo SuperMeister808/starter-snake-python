76224a897bfe5b77d36749e7ba981931642fc270
Emil Chatzistamatiou <emil.chatzistamatiou@gmail.com>
2026-02-16 14:46:14 +0100

    created test_load_is_move_safe

---
+++ tests/test_move/test_load_is_move_safe.py
@@ -0,0 +1,23 @@
+
+import unittest
+from unittest.mock import patch
+
+from move import Move
+
+class TestLoadIsMoveSafe(unittest.TestCase):
+
+    def setUp(self):
+        
+        self.bot = Move()
+    
+    def test_possible_scenario(self):
+
+        pass
+    
+    def test_regular_datatype(self):
+
+        pass
+
+    def test_irreglular_datatype(self):
+
+        pass
