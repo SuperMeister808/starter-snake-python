689e11c3edcc1674f26ce4c0a0b55641e99bf81c
Emil Chatzistamatiou <emil.chatzistamatiou@gmail.com>
2026-02-16 15:24:29 +0100

    test_check_moves created

---
+++ tests/test_move/test_check_moves.py
@@ -0,0 +1,23 @@
+
+import unittest
+from unittest.mock import patch
+
+from move import Move
+
+class TestCheckMoves(unittest.TestCase):
+
+    def setUp(self):
+        
+        self.bot = Move()
+
+    def test_call_emergency_system_no_return_value(self):
+
+        pass
+
+    def test_call_emergency_system_return_value(self):
+
+        pass
+
+    def test_call_emergency_system_return_value_during_iteration(self):
+
+        pass
