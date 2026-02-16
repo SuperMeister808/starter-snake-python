4e54e4bdd044908b093785e841ff0979a37ee46d
Emil Chatzistamatiou <emil.chatzistamatiou@gmail.com>
2026-02-16 13:44:53 +0100

    setup patchers + created_patcher_is_move_safe

--- tests/test_move/test_safe_is_move_safe.py
+++ tests/test_move/test_safe_is_move_safe.py
@@ -10,9 +10,40 @@ class TestSafeIsMoveSafe(unittest.TestCase):
 
         self.bot = Move()
     
-    def test_regular_dictionary(self):#
+    def setup_patchers(self, patcher_is_move_safe):
 
-        pass
+        self.patchers = []
+        
+        patcher_is_move_safe = self.create_patcher_is_move_safe(patcher_is_move_safe)
+        self.patchers.append(patcher_is_move_safe)
+
+        return self.patchers
+    
+    def start_patchers(self):
+
+        self.mocks = {}
+        
+        for i, patcher in enumerate(self.patchers):
+            mock = patcher.start()
+            self.mocks[i] = mock
+
+    def stop_patchers(self):
+
+        for patcher in self.patchers:
+            patcher.stop()
+
+        self.patchers.clear()
+        self.mocks.clear()
+    
+    def create_patcher_is_move_safe(self, patcher):
+
+        patcher_is_move_safe = patch.object(self.bot, "is_move_safe", new=patcher)
+        return patcher_is_move_safe
+    
+    def test_regular_dictionary(self):
+
+        patcher_is_move_safe = {"left": {}, "right": {}, "up": {}, "down": {}}
+        self.setup_patchers(patcher_is_move_safe)
 
     def test_irregular_datatype(self):
 
