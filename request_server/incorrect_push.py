c959f9f9598d1c4f9b59c06c09d4864901eecc8b
Emil Chatzistamatiou <emil.chatzistamatiou@gmail.com>
2026-02-17 15:41:29 +0100

    fixed incorrect_push

--- request_server/incorrect_push.py
+++ request_server/incorrect_push.py
@@ -1,7 +1,7 @@
 
 import requests
 
-headers = {"X-Admin-Token": "12345678"}
+headers = {"X-Admin-Token": "00000000"}
 response = requests.get(url="http://192.168.2.116:8000/admin/push", headers=headers)
 
 print(response.json())
