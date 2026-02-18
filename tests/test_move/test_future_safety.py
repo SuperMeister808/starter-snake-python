7069f95e0778102f6b3013b1a0066f7bee85b563
Emil Chatzistamatiou <emil.chatzistamatiou@gmail.com>
2026-02-18 19:26:53 +0100

    created test_future_safety

--- tests/test_move/test_future_safety.py
+++ tests/test_move/test_future_safety.py
@@ -6,7 +6,21 @@ from move import Move
 
 class TestFutureSafety(unittest.TestCase):
 
-    
+    def setUp(self):
+        
+        self.bot = Move()
+
+    def test_emergency_id(self):
+
+        pass
+
+    def test_is_safe_move_left(self):
+
+        pass
+
+    def test_no_safe_moves_left(self):
+
+        pass
 
 if __name__ == "__main__":
     unittest.main()
