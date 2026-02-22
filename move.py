065fd59b6cd1e5b6221aedfa1f48c8e148bb2540
Emil Chatzistamatiou <emil.chatzistamatiou@gmail.com>
2026-02-22 12:40:58 +0100

    bug-fix: snake needs to be a dict + flag listed added in check_datatypes

--- move.py
+++ move.py
@@ -445,33 +445,43 @@ class Move():
     
     def check_datatype(self, keywords: typing.Dict):
 
-        DICTIONARY_KEYS = ["head", "game_state", "neck", "new_head", "safe_moves"]
-        LIST_KEYS = ["body", "snake", "memory_moves"]
+        DICTIONARY_KEYS = ["head", "game_state", "neck", "new_head", "safe_moves", "snake"]
+        LIST_KEYS = ["body", "memory_moves"]
         STRING_KEYS = ["move"]
         INTEGER_KEYS = ["calls"]
         FLOAT_KEYS = []
 
         for key , keyword in keywords.items():
             
+            listed = False
+            
             if key in DICTIONARY_KEYS:
+                listed = True
                 if not isinstance(keyword, dict):
                     raise TypeError(f"{key} als Dictionary erforderlich!")
                 
             if key in LIST_KEYS:
+                listed = True
                 if not isinstance(keyword, list):
                     raise TypeError(f"{key} als Liste erforderlich!")
                 
             if key in STRING_KEYS:
+                listed = True
                 if not isinstance(keyword, str):
                     raise TypeError(f"{key} als String erforderlich!")
                 
             if key in INTEGER_KEYS:
+                listed = True
                 if not isinstance(keyword, int):
                     raise TypeError(f"{key} als Integer erforderlich!")
                 
             if key in FLOAT_KEYS:
+                listed = True
                 if not isinstance(keyword, float):
                     raise TypeError(f"{key} als Float erforderlich!")
+                
+            if listed == False:
+                raise RuntimeError(f"{key} nicht gelisted")
     
     def extract_keywords(self, needed_keywords: typing.List[str], **kwargs):
 
