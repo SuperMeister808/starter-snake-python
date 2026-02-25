488accd4c289e10711b55b1b2cb35f5207025c31
Emil Chatzistamatiou <emil.chatzistamatiou@gmail.com>
2026-02-25 23:17:43 +0100

    refactor emergency_system

--- emergency_system.py
+++ emergency_system.py
@@ -21,19 +21,12 @@ class EmergencySystem():
             return result
         except Exception as e:
             EmergencyLogger.loger_queue.put((func.__name__, e, self.move.turn_counter))
-            if func.__name__ == "reset_is_move_safe":
-                self.is_move_safe = {"left": {"is_safe": True, "priority": 0}, 
-                                 "right": {"is_safe": True, "priority": 0},
-                                 "up": {"is_safe": True, "priority": 0},
-                                 "down": {"is_safe": True, "priority": 0}}
-                return None
-            else:
-                try:
-                    next_move = random.choice(emergency_moves)
-                    return {"move": next_move, "id": "Emergency!"}
-                except Exception as e:
-                    EmergencyLogger.loger_queue.put((func.__name__, f"{e}", self.move.turn_counter))
-                    return {"move": "down", "id": "Emergency!"}
+            try:
+                next_move = random.choice(emergency_moves)
+                return {"move": next_move, "id": "Emergency!"}
+            except Exception as e:
+                EmergencyLogger.loger_queue.put((func.__name__, f"{e}", self.move.turn_counter))
+                return {"move": "down", "id": "Emergency!"}
                 
     def is_emergency(self, result):
 
