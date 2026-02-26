b05831211c2057855908b3d0eafba026333746d1
Emil Chatzistamatiou <emil.chatzistamatiou@gmail.com>
2026-02-26 16:15:33 +0100

    bug-fix: every appended node gets list children

--- future_safety_tree.py
+++ future_safety_tree.py
@@ -22,7 +22,7 @@ class FutureSafetyTree():
             raise KeyError("key children oder id nicht in parent vorhanden")
         child_id = parent_id + [len(parent_children) + 1] 
         
-        node = {"id": child_id, "head": head, "body": body, "neck": neck}
+        node = {"id": child_id, "head": head, "body": body, "neck": neck, "children": []}
         parent_children.append(node)
 
     def find_parent(self, id, parent=None, iteration_counter=None, max_depth=20):
