29fed0e976211b02452f9cb4eead3dffa26f3045
Emil Chatzistamatiou <emil.chatzistamatiou@gmail.com>
2026-02-26 22:22:09 +0100

    created test_future_safety_tree to find bug

---
+++ tests/test_future_safety_tree/test_future_safety_tree.py
@@ -0,0 +1,45 @@
+
+import unittest
+from unittest.mock import patch
+
+class TestFutureSafetyTree():
+
+    def test_find_parent(self):
+
+        pass
+
+    def test_find_parent_root(self):
+
+        pass
+    
+    def test_find_parent_too_many_iterations(self):
+
+        pass
+
+    def test_find_parent_id_not_found(self):
+
+        pass
+
+    def test_find_parent_keyerror_child_id(self):
+
+        pass
+
+    def test_find_parent_too_short_id(self):
+
+        pass
+    
+    def test_add_node(self):
+
+        pass
+
+    def test_add_node_wrong_keys(self):
+
+        pass
+
+    def test_add_node_wrong_parent_keys(self):
+
+        pass
+
+    def test_reset_tree(self):
+
+        pass
