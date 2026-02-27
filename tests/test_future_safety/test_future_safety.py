2ecc9a38c244685aaad9a086846d2e9727c00ccc
Emil Chatzistamatiou <emil.chatzistamatiou@gmail.com>
2026-02-27 17:58:49 +0100

    discover is_growing bug in FutureSafety during brainstorming about test scenarios

--- tests/test_future_safety/test_future_safety.py
+++ tests/test_future_safety/test_future_safety.py
@@ -6,7 +6,7 @@ class TestFutureSafety(unittest.TestCase):
 
     def test_safe_moves_left(self):
 
-        pass
+        game_state = {"you": {"head": {"x": 2, "y": 2}, "body": [{"x": 2, "y": 2}, {"x": 2, "y": 1}, {"x": 1, "y": 1}, {"x": 1, "y": 2}, {"x": 1, "y": 3}, {"x": 1, "y": 4}, {"x": 2, "y": 4}, {"x": 3, "y": 4}, {"x": 3, "y": 3}]}}
 
     def test_no_safe_moves_left(self):
 
