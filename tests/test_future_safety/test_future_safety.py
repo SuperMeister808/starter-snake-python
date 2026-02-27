84de599265cb120ae88d876c692b247c8389b6c4
Emil Chatzistamatiou <emil.chatzistamatiou@gmail.com>
2026-02-27 17:40:36 +0100

    created TestFutureSafety in ./tests/test_future_safety/test_future_safety.py

---
+++ tests/test_future_safety/test_future_safety.py
@@ -0,0 +1,13 @@
+
+import unittest
+from unittest.mock import patch
+
+class TestFutureSafety(unittest.TestCase):
+
+    def test_safe_moves_left(self):
+
+        pass
+
+    def test_no_safe_moves_left(self):
+
+        pass
