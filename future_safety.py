b344bec0f578d177492106f9bb2bd809d4ea858b
Emil Chatzistamatiou <emil.chatzistamatiou@gmail.com>
2026-03-02 17:28:07 +0100

    FutureSafety: future_safety: bug-fix: renamed relevant_positions into node_ids, node_ids appends nodes directly

--- future_safety.py
+++ future_safety.py
@@ -17,28 +17,26 @@ class FutureSafety():
         
         self.tree_id = [0]
 
-    def future_safety(self, relevant_positions=None, **kwargs):
+    def future_safety(self, node_ids=None, **kwargs):
             
             NEEDED_KEYWORDS = ["head", "game_state", "body", "neck", "my_length"]
 
-            if relevant_positions is None:
+            if node_ids is None:
                 head, game_state, body, neck, my_length = self.keywords.extract_keywords(NEEDED_KEYWORDS, **kwargs)
                 data = {"head": head, "body": body, "neck": neck, "my_length": my_length}
                 root_id = self.create_future_safety_tree(data)
                 self.log_data("future_safety", {"process": "create root form args", "data": data})
                 
-                positions = {"id": root_id}
-                relevant_positions = []
-                relevant_positions.append(positions)
-                self.log_data("future_safety", {"process": "added root into relevant_position"})
+                node_ids = []
+                node_ids.append(root_id)
+                self.log_data("future_safety", {"process": "added root into relevant_nodes"})
 
             safe_move_left = False
-            for e in relevant_positions[:]:
+            for node_id in node_ids[:]:
                 
                 self.reset_safe_moves()
                 
-                id = e["id"]
-                head , body , neck , length = self.extract_data_from_tree(id)
+                head , body , neck , length = self.extract_data_from_tree(node_id)
                 self.log_data("future_safety", {"process": "extract_data_from_tree", "head": head, "body": body, "neck": neck, "my_length": my_length})
                     
                 self.move.check_moves(self.safe_moves, head=head, game_state=game_state, body=body, neck=neck, my_length=length)
@@ -50,14 +48,14 @@ class FutureSafety():
                         move_possition = self.get_move(move, head)
                         new_body , new_neck , new_length = self.create_data_from_head(move_possition, body, length)
                         data = {"head": move_possition, "body": new_body, "neck": new_neck, "my_length": new_length}
-                        child_id = self.future_safety_tree.add_node(data, id)
-                        relevant_positions.append(child_id)
+                        child_id = self.future_safety_tree.add_node(data, node_id)
+                        node_ids.append(child_id)
                         self.log_data("future_safety", {"process": "add_node_safe_move", "head": move_possition, "body": new_body, "neck": new_neck, "my_length": my_length})
 
-                relevant_positions.remove(e)
+                node_ids.remove(node_id)
                 self.log_data("future_safety", {"process": "remove_current_node"})
 
-            return safe_move_left , relevant_positions
+            return safe_move_left , node_ids
     
     def extract_data_from_tree(self, id):
          
@@ -135,9 +133,9 @@ class FutureSafety():
             new_neck = self.move.get_neck(body=new_body)
 
             self.log_data("call_future_safety", {"process": "calculate body for move which will be simulated", "body": new_body, "neck": new_neck, "head": head, "my_length": my_length})
-            relevant_position = None
+            node_ids = None
             for i in range(calls):
-                safe_move_left , relevant_position = self.future_safety(relevant_position, head=head, game_state=game_state, body=new_body, neck=new_neck, my_length=my_length)
+                safe_move_left , node_ids = self.future_safety(node_ids, head=head, game_state=game_state, body=new_body, neck=new_neck, my_length=my_length)
 
             if safe_move_left == False:
                 return False
