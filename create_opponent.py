913e2abeb57d760bc2ca656b9aef528ef57bb55a
Emil Chatzistamatiou <emil.chatzistamatiou@gmail.com>
2026-03-03 18:19:34 +0100

    implemented log also for opponent

--- create_opponent.py
+++ create_opponent.py
@@ -5,5 +5,5 @@ from main import ServerHandler
 if __name__ == "__main__":
 
     handler = ServerHandler()
-    app = Server({"info": handler.info, "start": handler.start, "end": handler.end, "move": handler.move, "push": handler.push}, 5000)
+    app = Server({"info": handler.info, "start": handler.start, "end": handler.end, "move": handler.move, "push": handler.push}, 5000, "OpponentLogger", "opponent.log", False)
     app.run_server()
