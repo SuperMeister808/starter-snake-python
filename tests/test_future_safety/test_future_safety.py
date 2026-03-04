362f6f7013fbffac8d529d976a5c096769b1c6f1
Emil Chatzistamatiou <emil.chatzistamatiou@gmail.com>
2026-03-04 17:43:21 +0100

    TestFutureSafety: setup patchers

--- tests/test_future_safety/test_future_safety.py
+++ tests/test_future_safety/test_future_safety.py
@@ -1,9 +1,41 @@
 
 import unittest
-from unittest.mock import patch
+from unittest.mock import patch , MagicMock
 
-class TestFutureSafety():
+from future_safety import FutureSafety
 
+class TestFutureSafety(unittest.TestCase):
+
+    def setUp(self):
+
+        self.patchers = [
+            patch.object(FutureSafety, "log_data")
+        ]
+        self.mocks = {}
+
+        self.start_patchers()
+        self.addCleanup(self.stop_patchers)
+        
+    def start_patchers(self):
+
+        for i , patcher in enumerate(self.patchers):
+            mock = patcher.start()
+            try:
+                self.mocks [mock._mock_name] = mock
+            except AttributeError:
+                self.mocks [i] = mock
+
+    def stop_patchers(self):
+
+        for patcher in self.patchers:
+            patcher.stop()
+
+    def check_calls(self):
+
+        for name , mock in self.mocks.items():
+            if isinstance(mock, MagicMock):
+                mock.assert_called()
+    
     def test_safe_move_left(self):
 
         pass
