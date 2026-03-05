91337ea28b8cab572ebf0f7437930bb5150dc517
Emil Chatzistamatiou <emil.chatzistamatiou@gmail.com>
2026-03-05 16:42:36 +0100

    ./tests/test_future_safety/test_call_future_safety.py: creted TestCallFutureSafety

---
+++ tests/test_future_safety/test_call_future_safety.py
@@ -0,0 +1,22 @@
+
+import unittest
+from unittest.mock import patch
+
+class TestCallFutureSafety():
+
+    def test_2_calls_move_up(self):
+
+        pass
+
+    def test_2_call_move_left(self):
+
+        pass
+
+    def test_1_call_move_up(self):
+
+        pass
+
+    def test_1_call_move_left(self):
+
+        pass
+
