dff39a8c18b9357c3c3865f06720962d129a3e25
Emil Chatzistamatiou <emil.chatzistamatiou@gmail.com>
2026-03-05 23:17:15 +0100

    TestEmergencySystem: deleted test_emergency_system_reset_is_move_safe + instance EmergencySytem() added

--- tests/test_emergency_system/test_emergency_system.py
+++ tests/test_emergency_system/test_emergency_system.py
@@ -3,11 +3,15 @@ import unittest
 from unittest.mock import patch , MagicMock
 
 from emergency_logger import EmergencyLogger
+from emergency_system import EmergencySystem
 from move import Move
 
 class TestEmergencySystem(unittest.TestCase):
 
     def setUp(self):
+        
+        self.bot = Move()
+        self.emergency_system = EmergencySystem(self.bot)
         
         self.patchers = [
             patch.object(EmergencyLogger, "loger_queue")
@@ -33,64 +37,34 @@ class TestEmergencySystem(unittest.TestCase):
             patcher.stop()
 
     def test_correct_call(self):
-
-        bot = Move()
         
         func = MagicMock()
-        game_state = {"testing...": "testing..."}
         arg = "Testing..."
 
-        bot.emergency_system(game_state, func, arg, kwarg=True)
+        self.emergency_system.emergency_system(func, arg, kwarg=True)
 
         func.assert_called_once_with(arg, kwarg=True)
 
         self.mock_loger_queue.put.assert_not_called()
 
-    def test_exception_reset_is_move_safe(self):
-
-        bot = Move()
-
-        game_state = {"testing...": "testing..."}
-        func = MagicMock()
-        func.__name__ = "reset_is_move_safe"
-        exc = RuntimeError("side effect")
-        func.side_effect = exc
-        arg = "Testing..."
-        kwarg = True
-
-        with patch.object(bot, "is_move_safe", new={}):
-        
-            bot.emergency_system(game_state, func, arg, kwarg=kwarg)
-
-            func.assert_called_once_with(arg, kwarg=kwarg)
-            self.mock_loger_queue.put.assert_called_once_with((func.__name__, exc, game_state))
-
-            self.assertEqual(bot.is_move_safe, {"left": {"is_safe": True, "priority": 0}, 
-                                "right": {"is_safe": True, "priority": 0},
-                                 "up": {"is_safe": True, "priority": 0},
-                                 "down": {"is_safe": True, "priority": 0}})
-
-    @patch("move.random.choice")
     def test_exception(self, mock_choice):
 
-        bot = Move()
-        
-        game_state = {"testing...": "testing..."}
         func = MagicMock()
         func.__name__ = "test_func"
         exc = RuntimeError("side effect")
         func.side_effect = exc
         arg = "Testing..."
         kwarg = True
-
-        mock_choice.return_value = "left"
         
-        result = bot.emergency_system(game_state, func, arg, kwarg=kwarg)
+        emergency_moves = ["left", "right", "down", "up"]
+        result = self.emergency_system.emergency_system(func, arg, kwarg=kwarg)
         
         func.assert_called_once_with(arg, kwarg=kwarg)
-        self.mock_loger_queue.put.assert_called_once_with((func.__name__, exc, game_state))
-        mock_choice.assert_called_once_with(["left", "right", "up", "down"])
-        self.assertEqual(result, {"move": "left"})
+        self.mock_loger_queue.put.assert_called_once_with((func.__name__, exc, self.bot.turn_counter, 40))
+        mock_choice.assert_called_once_with(emergency_moves)
+        
+        result_move = result["move"]
+        self.assertIn(result_move, emergency_moves)
 
 
 if __name__ == "__main__":
