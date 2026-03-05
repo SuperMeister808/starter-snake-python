76256607c89e936f978c6a07a4a0fba7b2ce28b6
Emil Chatzistamatiou <emil.chatzistamatiou@gmail.com>
2026-03-05 17:31:53 +0100

    TestCallFutureSafety: test_1_call_move_left: added

--- tests/test_future_safety/test_call_future_safety.py
+++ tests/test_future_safety/test_call_future_safety.py
@@ -104,7 +104,11 @@ class TestCallFutureSafety(unittest.TestCase):
 
     def test_1_call_move_left(self):
 
-        pass
+        self.setup_patchers("left", True)
+        result = self.future_safety.call_future_safety(1, game_state=self.game_state, body=self.body, move=self.move, head=self.head, my_length=self.my_length, neck=self.neck)
+        self.check_calls()
+
+        self.assertTrue(result)
 
 if __name__ == "__main__":
 
