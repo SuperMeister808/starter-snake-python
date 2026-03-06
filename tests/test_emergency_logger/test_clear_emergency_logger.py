3cca1812b2685cd54dfb2b08e9a6284138b937a9
Emil Chatzistamatiou <emil.chatzistamatiou@gmail.com>
2026-03-06 18:38:48 +0100

    TestClearEmergencyLogger: test_clear_emergency_logger: decorators added

--- tests/test_emergency_logger/test_clear_emergency_logger.py
+++ tests/test_emergency_logger/test_clear_emergency_logger.py
@@ -2,8 +2,18 @@
 import unittest
 from unittest.mock import patch
 
-class TestClearEmergencyLogger():
+from emergency_logger import EmergencyLogger
+from runtime_logger import RuntimeLogger
 
-    def test_clear_emergency_logger(self):
+class TestClearEmergencyLogger(unittest.TestCase):
 
-        pass
+    @patch.object(EmergencyLogger, "flags", new={})
+    @patch.object(EmergencyLogger, "runtime_logger", new="runtime_logger")
+    @patch.object(RuntimeLogger, "close_file_handlers")
+    @patch.object(EmergencyLogger.print_collector, "clear_messages")
+    def test_clear_emergency_logger(self, mock_print_collector_clear_messages, mock_close_file_handlers):
+
+        EmergencyLogger.clear_emergency_logger()
+
+
+
