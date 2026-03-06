9d2fb4a3b9e79ab84fb7b4963799501b0265ae30
Emil Chatzistamatiou <emil.chatzistamatiou@gmail.com>
2026-03-06 18:55:16 +0100

    TestEmergencyLog: added patches, type(return_value)=tuple

--- tests/test_emergency_logger/test_emergency_log.py
+++ tests/test_emergency_logger/test_emergency_log.py
@@ -2,8 +2,17 @@
 import unittest
 from unittest.mock import patch
 
-class TestEmergencyLog():
+from emergency_logger import EmergencyLogger
 
+class TestEmergencyLog(unittest.TestCase):
+
+    def setUp(self):
+        
+        self.patchers = [
+            patch.object(EmergencyLogger, "create_message", return_value=lambda cls, where, exception: (where, exception)),
+            patch.object(EmergencyLogger, "log", return_value=lambda level, message, extra: (level, message, extra))
+        ]
+    
     def test_default_message(self):
 
         pass
