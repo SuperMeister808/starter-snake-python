97e8e22b7b28f45ba45b8ba952d859a3f6ddfeb5
Emil Chatzistamatiou <emil.chatzistamatiou@gmail.com>
2026-03-06 19:00:59 +0100

    TestEmergencyLog: simple start-stop-system added

--- tests/test_emergency_logger/test_emergency_log.py
+++ tests/test_emergency_logger/test_emergency_log.py
@@ -1,6 +1,6 @@
 
 import unittest
-from unittest.mock import patch
+from unittest.mock import patch , MagicMock
 
 from emergency_logger import EmergencyLogger
 
@@ -8,11 +8,43 @@ class TestEmergencyLog(unittest.TestCase):
 
     def setUp(self):
         
+        self.mocks = {}
         self.patchers = [
             patch.object(EmergencyLogger, "create_message", return_value=lambda cls, where, exception: (where, exception)),
             patch.object(EmergencyLogger, "log", return_value=lambda level, message, extra: (level, message, extra))
         ]
+
+        self.addCleanup(self.stop_patchers)
     
+    def start_patchers(self):
+
+        for i , patcher in enumerate(self.patchers):
+            mock = patcher.start()
+            try:
+                self.mocks [mock._mock_name] = mock
+            except AttributeError:
+                if not isinstance(mock, MagicMock):
+                    self.mocks [i] = mock
+                else:
+                    raise
+
+    def stop_patchers(self):
+
+        for patcher in self.patchers:
+            patcher.stop()
+
+    def check_calls(self):
+
+        for name , mock in self.mocks.items():
+
+            try:
+                mock.assert_called()
+            except AttributeError:
+                if not isinstance(mock, MagicMock):
+                    pass
+                else:
+                    raise
+
     def test_default_message(self):
 
         pass
