94d3eabdfff5ff1c91eced9c2efd47df55b866bc
Emil Chatzistamatiou <emil.chatzistamatiou@gmail.com>
2026-03-08 09:39:38 +0100

    TestGetAllowedKeywords: patched keyword lists + test_only_allowed_keywords: added

--- tests/test_keywords/test_get_allowed_keywords.py
+++ tests/test_keywords/test_get_allowed_keywords.py
@@ -2,11 +2,25 @@
 import unittest
 from unittest.mock import patch
 
-class TestGetAllowedKeywords():
+from keywords import Keywords
 
+class TestGetAllowedKeywords(unittest.TestCase):
+
+    keywords = Keywords()
+
+    @patch.object(keywords, "ALLOWED_KEYWORDS", new=["head", "body", "neck", "my_length"])
     def test_only_allowed_keywords(self):
 
-        pass
+        result = self.keywords.get_allowed_keywords(head="head", body="body", neck="neck", my_length="my_length")
+        head = result["head"]
+        body = result["body"]
+        neck = result["neck"]
+        my_length = result["my_length"]
+
+        self.assertEqual(head, "head")
+        self.assertEqual(body, "body")
+        self.assertEqual(neck, "neck")
+        self.assertEqual(my_length, "my_length")
 
     def test_sort_unallowed_keywords_out(self):
 
@@ -15,3 +29,6 @@ class TestGetAllowedKeywords():
     def test_only_unallowed_keywords(self):
 
         pass
+
+if __name__ == "__main__":
+    unittest.main()
