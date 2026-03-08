edfaeac9f5c14f30f9190ccc425929c250ed7736
Emil Chatzistamatiou <emil.chatzistamatiou@gmail.com>
2026-03-08 15:32:47 +0100

    TestCalculateFood: test_food_out_of_range: added

--- tests/test_move/test_calculate_food.py
+++ tests/test_move/test_calculate_food.py
@@ -73,7 +73,15 @@ class TestCalculateFood(unittest.TestCase):
 
     def test_food_out_of_range(self):
 
-        pass
+        game_state = {"board": {"food": [{"x": 4, "y": 4}]}}
+        head = {"x": 2, "y": 2}
+
+        self.bot.calculate_food(self.is_move_safe, head=head, game_state=game_state)
+
+        self.assertEqual(self.is_move_safe["left"]["priority"], 0)
+        self.assertEqual(self.is_move_safe["right"]["priority"], 0)
+        self.assertEqual(self.is_move_safe["up"]["priority"], 0)
+        self.assertEqual(self.is_move_safe["down"]["priority"], 0)
 
     def test_food_in_range_and_food_out_of_range(self):
 
