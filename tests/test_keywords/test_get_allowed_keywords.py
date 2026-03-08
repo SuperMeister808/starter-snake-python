6fbc1a6b96e7f1732de7c21e408f5b3dfbfe75fc
Emil Chatzistamatiou <emil.chatzistamatiou@gmail.com>
2026-03-08 09:27:11 +0100

    ./tests/test_keywords/test_get_allowed_keywords.py: TestGetAllowedKeywords added

---
+++ tests/test_keywords/test_get_allowed_keywords.py
@@ -0,0 +1,17 @@
+
+import unittest
+from unittest.mock import patch
+
+class TestGetAllowedKeywords():
+
+    def test_only_allowed_keywords(self):
+
+        pass
+
+    def test_sort_unallowed_keywords_out(self):
+
+        pass
+
+    def test_only_unallowed_keywords(self):
+
+        pass
