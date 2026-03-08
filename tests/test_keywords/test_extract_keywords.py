160cd78c1b1ae8155535e58bb0ad1f594bdd9266
Emil Chatzistamatiou <emil.chatzistamatiou@gmail.com>
2026-03-08 13:18:28 +0100

    ./tests/test_keywords/test_extract_keywords: TstExtractKeywords created

---
+++ tests/test_keywords/test_extract_keywords.py
@@ -0,0 +1,21 @@
+
+import unittest
+from unittest.mock import patch
+
+class TestExtractKeywords():
+
+    def test_correct_needed_keywords(self):
+
+        pass
+
+    def test_unnecessary_keywords(self):
+
+        pass
+
+    def test_needed_keywords_missing(self):
+
+        pass
+
+    def test_needed_keywords_missing_and_unnecessary_keywords(self):
+
+        pass
