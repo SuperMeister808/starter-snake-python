7aea097e35b7a3d48e1afe422165788992e8f44e
Emil Chatzistamatiou <emil.chatzistamatiou@gmail.com>
2026-03-08 13:32:42 +0100

    TestExtractKeywords: setUp: patcher check_datatype

--- tests/test_keywords/test_extract_keywords.py
+++ tests/test_keywords/test_extract_keywords.py
@@ -1,9 +1,46 @@
 
 import unittest
-from unittest.mock import patch
+from unittest.mock import patch , MagicMock
 
-class TestExtractKeywords():
+from keywords import Keywords
 
+class TestExtractKeywords(unittest.TestCase):
+
+    keywords = Keywords()
+    def setUp(self):
+        
+        self.patchers = [
+            patch.object(self.keywords, "check_datatype", name="mock_check_datatype")
+        ]
+        self.mocks = {}
+
+        self.start_patchers()
+        self.addCleanup(self.stop_patchers)
+
+    def start_patchers(self):
+
+        for i , patcher in enumerate(self.patchers):
+            mock = patcher.start()
+            try:
+                self.mocks [mock.name] = mock
+            except AttributeError:
+                if isinstance(mock, MagicMock):
+                    self.mocks [mock._mock_name] = mock
+                else:
+                    self.mocks [i] = mock
+
+    def stop_patchers(self):
+
+        for patcher in self.patchers:
+            patcher.stop()
+
+    def check_calls(self):
+
+        for name , mock in self.mocks.items():
+            if not isinstance(mock, MagicMock):
+                continue
+            mock.assert_called()
+    
     def test_correct_needed_keywords(self):
 
         pass
