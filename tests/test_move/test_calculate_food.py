80dd15216fa7ef3a5e570f5eb5a03a1ade30f49e
Emil Chatzistamatiou <emil.chatzistamatiou@gmail.com>
2026-03-08 15:22:48 +0100

    TestCalculateFood: test_food_move_left: added

--- tests/test_move/test_calculate_food.py
+++ tests/test_move/test_calculate_food.py
@@ -6,9 +6,22 @@ from move import Move
 
 class TestCalculateFood(unittest.TestCase):
 
+    def setUp(self):
+        
+        self.bot = Move()
+        self.is_move_safe = {"left": {"priority": 0}, "right": {"priority": 0}, "up": {"priority": 0}, "down": {"priority": 0}}
+    
     def test_food_move_left(self):
 
-        pass
+        game_state = {"board": {"food": [{"x": 2, "y": 2}]}}
+        head = {"x": 3, "y": 2}
+
+        self.bot.calculate_food(self.is_move_safe, head=head, game_state=game_state)
+
+        self.assertEqual(self.is_move_safe["left"]["priority"], 1)
+        self.assertEqual(self.is_move_safe["right"]["priority"], 0)
+        self.assertEqual(self.is_move_safe["up"]["priority"], 0)
+        self.assertEqual(self.is_move_safe["down"]["priority"], 0)
 
     def test_food_move_right(self):
 
