bafa7d9a06e732b9703cee00c2d78b9ea581173f
Emil Chatzistamatiou <emil.chatzistamatiou@gmail.com>
2026-03-08 15:07:49 +0100

    TestCalculateFood: created

--- tests/test_move/test_calculate_food.py
+++ tests/test_move/test_calculate_food.py
@@ -6,7 +6,33 @@ from move import Move
 
 class TestCalculateFood(unittest.TestCase):
 
-    pass
+    def test_food_move_left(self):
+
+        pass
+
+    def test_food_move_right(self):
+
+        pass
+
+    def test_food_move_up(self):
+
+        pass
+
+    def test_food_move_down(self):
+
+        pass
+
+    def test_food_on_multiple_moves(self):
+
+        pass
+
+    def test_food_out_of_range(self):
+
+        pass
+
+    def test_food_in_range_and_food_out_of_range(self):
+
+        pass
 
 if __name__ == "__main__":
 
