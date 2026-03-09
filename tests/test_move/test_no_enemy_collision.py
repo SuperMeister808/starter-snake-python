f8eacc84ee54fb93150bc22035fe09fe252a209a
Emil Chatzistamatiou <emil.chatzistamatiou@gmail.com>
2026-03-09 23:16:34 +0100

    TestNoEnemyCollision: move_assertions: helper function to assert bool is_safe and int priority added

--- tests/test_move/test_no_enemy_collision.py
+++ tests/test_move/test_no_enemy_collision.py
@@ -8,6 +8,10 @@ class TestNoEnemyCollision(unittest.TestCase):
     bot = Move()
     def setUp(self):
         
+        self.is_move_safe =  {"up": {"is_safe": True, "priority": 0}, 
+                             "down": {"is_safe": True, "priority": 0}, 
+                             "left": {"is_safe": True, "priority": 0}, 
+                             "right": {"is_safe": True, "priority": 0}}
         self.head = {"x": 2, "y": 2}
         self.game_state = "..."
         self.patchers = [
@@ -40,9 +44,37 @@ class TestNoEnemyCollision(unittest.TestCase):
         
         mock_extract_keywords.assert_called_once_with(self.head, self.game_state)
     
+    def extract_moves(self):
+        
+        left = self.is_move_safe.get("left", "unknown")
+        right = self.is_move_safe.get("right", "unknown")
+        down = self.is_move_safe.get("down", "unknwon")
+        up = self.is_move_safe.get("up", "unknwon")
+
+        return left , right , down , up
+    
+    def move_assertions(self, left_safe, left_priority, right_safe, right_priority, down_safe, down_priority, up_safe, up_priority):
+        
+        left , right , down , up = self.extract_moves()
+        self.assertEqual(left.get("is_safe", "unknown"), left_safe)
+        self.assertEqual(right.get("is_safe", "unknown"), right_safe)
+        self.assertEqual(down.get("is_safe", "unknown"), down_safe)
+        self.assertEqual(up.get("is_safe", "unknown"), up_safe)
+        self.assertEqual(left.get("priority", "unknown"), left_priority)
+        self.assertEqual(right.get("priority", "unknown"), right_priority)
+        self.assertEqual(down.get("priority", "unknown"), down_priority)
+        self.assertEqual(up.get("priority", "unknown"), up_priority)
+
+    
+    @patch.object(bot, "opponents_positions", new={"...": {"unsafe": [{"x": 3, "y": 2}, {""}]}})
     def test_unsafe_moves(self):
         
-        pass
+        new_opponents_positions = {"...": {"unsafe": [{"x": 3, "y": 2}, {"x": 1, "y": 2}]}}
+        with patch.object(self.bot, "opponents_positions", new=new_opponents_positions):
+            self.bot.not_enemy_collision(self.is_move_safe, head=self.head, game_state=self.game_state)
+            left , right , down , up = self.extract_moves()
+            self.ass
+
     
     def test_priority_moves(self):
         
@@ -53,6 +85,10 @@ class TestNoEnemyCollision(unittest.TestCase):
         pass
     
     def test_no_unsafe_and_no_priority_moves(self):
+        
+        pass
+    
+    def test_spread_entries_over_multiple_snakes(self):
         
         pass
     
