2a69267083affc037e42ce7a308ea628af31d42c
Emil Chatzistamatiou <emil.chatzistamatiou@gmail.com>
2026-03-09 15:55:32 +0100

    TestChooseMove: test_no_fallback: added

--- tests/test_move/test_choose_move.py
+++ tests/test_move/test_choose_move.py
@@ -8,13 +8,12 @@ from move import Move
 class TestChooseMove(unittest.TestCase):
 
     bot = Move()
+    neck = {}
+    head = {}
+    body = []
+    my_length = 0
+    game_state = {"you": {"head": head, "body": body, "length": my_length}}
     def setUp(self):
-        
-        self.game_state = {}
-        self.head = {}
-        self.body = []
-        self.neck = {}
-        self.my_length = 0
         
         #Needed to be patched individualy
             #patch.object(self.bot, "edit_body", return_value="body"),
@@ -23,7 +22,7 @@ class TestChooseMove(unittest.TestCase):
         
         self.patchers = [
             patch.object(self.bot, "reset_is_move_safe", name="reset_is_move_safe"),
-            patch.object(self.bot, "log_data", name="log_data"),
+            patch.object(self.bot.future_safety, "log_data", name="log_data"),
             patch.object(self.bot, "check_safe_moves", name="check_safe_moves"),
             patch.object(self.bot, "check_priority_moves", name="check_priority_moves"),
             patch.object(self.bot, "random_choice", return_value={"move": "left"}, name="random_choice")
@@ -65,7 +64,7 @@ class TestChooseMove(unittest.TestCase):
                 raise TypeError("Objekt ist kein MagicMock")
             mock.assert_called()
 
-    def assert_calls_log_data(self):
+    def assert_calls_log_data(self, head, body, neck, my_length):
 
         try:
             mock_log_data = self.mocks ["log_data"]
@@ -74,9 +73,9 @@ class TestChooseMove(unittest.TestCase):
         if not isinstance(mock_log_data, MagicMock):
             raise TypeError("Objekt nicht als MagicMock vorhanden")
         self.assertEqual(mock_log_data.call_count, 5)
-        mock_log_data.assert_called_with("choose_move", {"head": self.head, "body": self.body, "neck": self.neck, "my_length": self.my_length})
+        mock_log_data.assert_called_with("choose_move", {"head": head, "neck": neck, "body": body, "my_length": my_length})
 
-    def assert_calls_check_safe_moves(self):
+    def assert_calls_check_safe_moves(self, head, game_state, body, neck, my_length):
 
         try:
             mock_check_safe_moves = self.mocks ["check_safe_moves"]
@@ -85,14 +84,25 @@ class TestChooseMove(unittest.TestCase):
         if not isinstance(mock_check_safe_moves, MagicMock):
             raise TypeError("Objekt nicht als MagicMock vorhanden")
         
-        mock_check_safe_moves.assert_called_once_with(ANY, head=self.head, game_state=self.game_state, body=self.body, neck=self.neck, my_length=self.my_length)
+        mock_check_safe_moves.assert_called_once_with(ANY, head=head, game_state=game_state, body=body, neck=neck, my_length=my_length)
     
-    @patch.object
-    def test_no_fallback(self):
+    @patch.object(bot, "check_moves")
+    @patch.object(bot, "get_neck", return_value=neck)
+    @patch.object(bot, "edit_body", return_value=body)
+    def test_no_fallback(self, mock_edit_body, mock_get_neck, mock_check_moves):
 
-        pass
+        result = self.bot.choose_move(self.game_state)
+        self.assertEqual(result, {"move": "left"})
 
-    def test_fallback_extract_game_state(self):#
+        self.assert_calls_no_args()
+        self.assert_calls_log_data(self.head, self.body, self.neck, self.my_length)
+        self.assert_calls_check_safe_moves(self.head, self.game_state, self.body, self.neck, self.my_length)
+
+        mock_edit_body.assert_called_once_with(self.body)
+        mock_get_neck.assert_called_once_with(body=self.body, game_state=self.game_state)
+        mock_check_moves.assert_called_once_with(self.bot.is_move_safe, head=self.head, game_state=self.game_state, body=self.body, neck=self.neck, my_length=self.my_length)
+
+    def test_fallback_extract_game_state(self):
 
         pass
 
