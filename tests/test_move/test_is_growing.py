ad9c34dec47216321815723cbeb66cbf20032e64
Emil Chatzistamatiou <emil.chatzistamatiou@gmail.com>
2026-03-09 22:44:29 +0100

    TestIsGrowing: test_is_not_growing_food_out_of_range: added

--- tests/test_move/test_is_growing.py
+++ tests/test_move/test_is_growing.py
@@ -42,6 +42,16 @@ class TestIsGrowing(unittest.TestCase):
 
         mock_extract_keywords.assert_called_once()
 
+  def test_is_not_growing_food_out_of_range(self):
+     
+     game_state = {"board": {"food": [{"x": 4, "y": 4}, {"x": 0, "y": 0}, {"x": 0, "y": 4}, {"x": 4, "y": 0}]}}
+     with patch.object(self.bot.keywords, "extract_keywords", return_value=(self.head, game_state)) as mock_extract_keywords:
+        result = self.bot.is_growing(head=self.head, game_state=game_state)
+        self.assertFalse(result)
+
+        mock_extract_keywords.assert_called_once()
+
+
 if __name__ == "__main__":
 
     unittest.main()
