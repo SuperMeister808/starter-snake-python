b2b57f00d8f355dc00015392b22287948064b0eb
Emil Chatzistamatiou <emil.chatzistamatiou@gmail.com>
2026-03-10 00:21:38 +0100

    ./tests/test_move/test_not_wall_collision.py: TestNotWallCollision: added

--- tests/test_move/test_not_wall_collision.py
+++ tests/test_move/test_not_wall_collision.py
@@ -5,7 +5,17 @@ from move import Move
 
 class TestNotWallCollision(unittest.TestCase):
 
+    def test_not_wall_collision_width(self):
+        
+        pass
     
+    def test_not_wall_collision_heigth(self):
+        
+        pass
+    
+    def test_not_wall_collision_corner(self):
+        
+        pass
 
 if __name__ == "__main__":
 
