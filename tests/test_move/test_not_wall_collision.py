de41fe727ac3a418cd391fde8406061792bff5c3
Emil Chatzistamatiou <emil.chatzistamatiou@gmail.com>
2026-03-10 00:25:53 +0100

    TestNotWallCollision: setUp: args + Move()

--- tests/test_move/test_not_wall_collision.py
+++ tests/test_move/test_not_wall_collision.py
@@ -5,6 +5,17 @@ from move import Move
 
 class TestNotWallCollision(unittest.TestCase):
 
+    bot = Move()
+    def setUp(self):
+        
+        self.is_move_safe = {"up": {"is_safe": True, "priority": 0}, 
+                             "down": {"is_safe": True, "priority": 0}, 
+                             "left": {"is_safe": True, "priority": 0}, 
+                             "right": {"is_safe": True, "priority": 0}}
+        
+        self.game_state = {"board": {"width": 11, "heigth": 11}}
+        self.head = {}
+    
     def test_not_wall_collision_width(self):
         
         pass
