8282996fceb74048b1f422be9810cf4b57475a90
Emil Chatzistamatiou <emil.chatzistamatiou@gmail.com>
2026-03-09 15:34:33 +0100

    TestChooseMove: added call assertion methods for setUp patchers

--- tests/test_move/test_choose_move.py
+++ tests/test_move/test_choose_move.py
@@ -1,7 +1,7 @@
 
 import unittest
 
-from unittest.mock import patch , MagicMock
+from unittest.mock import patch , MagicMock , ANY
 
 from move import Move
 
@@ -9,6 +9,12 @@ class TestChooseMove(unittest.TestCase):
 
     bot = Move()
     def setUp(self):
+        
+        self.game_state = {}
+        self.head = {}
+        self.body = []
+        self.neck = {}
+        self.my_length = 0
         
         #Needed to be patched individualy
             #patch.object(self.bot, "edit_body", return_value="body"),
@@ -46,16 +52,40 @@ class TestChooseMove(unittest.TestCase):
 
     def assert_calls_no_args(self):
 
-        mock_reset_is_move_safe = self.mocks ["reset_is_move_safe"]
-        mock_check_priority_moves = self.mocks ["check_priority_moves"]
-        mock_random_choice = self.mocks ["random_choice"]
+        try:
+            mock_reset_is_move_safe = self.mocks ["reset_is_move_safe"]
+            mock_check_priority_moves = self.mocks ["check_priority_moves"]
+            mock_random_choice = self.mocks ["random_choice"]
+            asserted_mocks = [mock_reset_is_move_safe, mock_check_priority_moves, mock_random_choice]
+        except KeyError:
+            raise KeyError("MagicMock Objekte nicht vorhanden")
 
-        mock_reset_is_move_safe.assert_called_once()
-        mock_check_priority_moves.assert_called_once()
-        mock_random_choice.assert_called_once()
+        for mock in asserted_mocks:
+            if not isinstance(mock, MagicMock):
+                raise TypeError("Objekt ist kein MagicMock")
+            mock.assert_called()
 
-    
+    def assert_calls_log_data(self):
 
+        try:
+            mock_log_data = self.mocks ["log_data"]
+        except KeyError:
+            raise KeyError("mock_log_data nicht vorhanden")
+        if not isinstance(mock_log_data, MagicMock):
+            raise TypeError("Objekt nicht als MagicMock vorhanden")
+        self.assertEqual(mock_log_data.call_count, 5)
+        mock_log_data.assert_called_with("choose_move", {"head": self.head, "body": self.body, "neck": self.neck, "my_length": self.my_length})
+
+    def assert_calls_check_safe_moves(self):
+
+        try:
+            mock_check_safe_moves = self.mocks ["check_safe_moves"]
+        except KeyError:
+            raise KeyError("mock_check_safe_moves nicht vorhanden")
+        if not isinstance(mock_check_safe_moves, MagicMock):
+            raise TypeError("Objekt nicht als MagicMock vorhanden")
+        
+        mock_check_safe_moves.assert_called_once_with(ANY, head=self.head, game_state=self.game_state, body=self.body, neck=self.neck, my_length=self.my_length)
     
     @patch.object
     def test_no_fallback(self):
