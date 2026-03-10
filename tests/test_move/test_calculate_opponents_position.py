2b55bf06b1b0a8fc5c8efeb799ba6b723fead66f
Emil Chatzistamatiou <emil.chatzistamatiou@gmail.com>
2026-03-10 21:26:53 +0100

    ./tests/test_move/test_calculate_opponents_positions.py: TestCalculateOpponentsPositions: created

--- tests/test_move/test_calculate_opponents_position.py
+++ tests/test_move/test_calculate_opponents_position.py
@@ -1,1 +1,30 @@
 
+import unittest
+from unittest.mock import patch
+
+class TestCalculateOpponentsPositions():
+
+    def test_extract_positions_one_snake(self):
+
+        pass
+
+    def test_extract_positions_one_snake_is_growing(self):
+
+        pass
+
+    def test_extract_positions_multiple_snakes(self):
+
+        pass
+
+    def test_extract_positions_no_snakes(self):
+
+        pass
+
+    def test_missing_body_parts_between_head_and_tail(self):
+
+        pass
+
+    #Edge-Case
+    def test_only_head(self):
+
+        pass
