d564d65243224941d506b0838b8bcec51c577f35
Emil Chatzistamatiou <emil.chatzistamatiou@gmail.com>
2026-03-10 21:51:56 +0100

    TestCalculateOpponentsPositions: setUp: setup patchers

--- tests/test_move/test_calculate_opponents_position.py
+++ tests/test_move/test_calculate_opponents_position.py
@@ -1,9 +1,40 @@
 
 import unittest
-from unittest.mock import patch
+from unittest.mock import patch , MagicMock
 
-class TestCalculateOpponentsPositions():
+from move import Move
 
+class TestCalculateOpponentsPositions(unittest.TestCase):
+
+    bot = Move()
+    def setUp(self):
+        
+        self.mocks = {}
+        self.patchers = [
+            patch.object(self.bot.future_safety, "log_data", name="mock_log_data"),
+            patch.object(self.bot, "reset_opponents_positions", name="mock_reset_opponents_positions")
+        ]
+
+        self.start_patchers()
+        self.addCleanup(self.stop_patchers)
+
+    def start_patchers(self):
+
+        for patcher in self.patchers:
+            mock = patcher.start()
+            if isinstance(mock, MagicMock):
+                self.mocks [mock._mock_name] = mock
+
+    def stop_patchers(self):
+        for patcher in self.patchers:
+            patcher.stop()
+    
+    def general_call_assertion(self):
+
+        for name , mock in self.mocks.items():
+            if isinstance(mock, MagicMock):
+                mock.assert_called()
+    
     def test_extract_positions_one_snake(self):
 
         pass
