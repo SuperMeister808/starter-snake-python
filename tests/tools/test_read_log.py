182fe1f24241f0b0012eae7348b60139f8bb2321
Emil Chatzistamatiou <emil.chatzistamatiou@gmail.com>
2026-03-14 13:16:25 +0100

    TestReadLogs: setUp: listed patchers and collect mocks in self.mocks

--- tests/tools/test_read_log.py
+++ tests/tools/test_read_log.py
@@ -1,5 +1,29 @@
-class TestReadLog():
+from unittest import TestCase
+from unittest.mock import patch, MagicMock , ANY
+from unittest import main
+from tools.log_analyzer import LogAnalyzer
+class TestReadLog(TestCase):
 
+    log_analyzer = LogAnalyzer()
+    def setUp(self):
+        self.patchers = [
+            patch.object(self.log_analyzer, "create_contents")
+        ]
+        self.mocks = {}
+
+        self.start_patchers()
+        self.addCleanup(self.stop_patcherss)
+
+    def start_patchers(self):
+        for patcher in self.patchers:
+            mock = patcher.start()
+            if isinstance(mock, MagicMock):
+                self.mocks [mock._mock_name] = mock
+
+    def stop_patcherss(self):
+        for pacher in self.patchers:
+            pacher.stop()
+    
     def test_read_log_default_args(self):
         
         pass
