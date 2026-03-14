250b0f77668578ed5b9fdb00399e7d6bcd4a8acb
Emil Chatzistamatiou <emil.chatzistamatiou@gmail.com>
2026-03-14 13:38:22 +0100

    ~/tests/test_tools/test_create_contents.py: TestCreateContents added

---
+++ tests/test_tools/test_create_contents.py
@@ -0,0 +1,21 @@
+from unittest import TestCase
+from unittest.mock import patch , MagicMock , ANY
+from unittest import main
+class TestCreateContents():
+
+    def test_none_args(self):
+
+        pass
+
+    def test_args(self):
+
+        pass
+
+    def test_too_high_index(self):
+
+        pass
+
+    #Edge-Cae
+    def test_len_words_equals_0(self):
+
+        pass
