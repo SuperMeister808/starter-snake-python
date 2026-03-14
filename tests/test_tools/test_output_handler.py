dcee7e4caf2931f5485fc334476c314b4c2e3949
Emil Chatzistamatiou <emil.chatzistamatiou@gmail.com>
2026-03-14 17:44:30 +0100

    ~/tests/test_tools/test_output_handler.py: TestOutputHandler created

---
+++ tests/test_tools/test_output_handler.py
@@ -0,0 +1,21 @@
+from unittest import TestCase
+from unittest.mock import patch , MagicMock , ANY
+from unittest import main
+
+class TestOutputFormat():
+
+    def test_output_format_file_correct_file_handler(self):
+
+        pass
+
+    def test_output_format_file_incorrect_file_handler(self):
+
+        pass
+
+    def test_output_format_console(self):
+
+        pass
+
+    def test_unallowed_output_format(self):
+
+        pass
