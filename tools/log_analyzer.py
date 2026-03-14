9db06868996ecefa93c7965b1bb918d031d79676
Emil Chatzistamatiou <emil.chatzistamatiou@gmail.com>
2026-03-14 12:14:20 +0100

    LogAnalyzer: refactoring read logs so that contents are not fixed

--- tools/log_analyzer.py
+++ tools/log_analyzer.py
@@ -1,39 +1,45 @@
 class LogAnalyzer():
 
     def __init__(self):
-        self.ALLOWED_ERRORS = [
-           "random_choice: Choosed emergency move"
-        ]
         self.contents = {}
 
-    def read_log(self, file):
+    def read_log(self, file, level_index=None, turn_index=None, log_index=None):
         
         with open(file) as f:
             for i, line in enumerate(f):
                 words = []
                 for word in line.split():
                     words.append(word)
-                level = words[0]
-                turn = word[2]
-                log = word[-1]
-                content = {"level": level, "turn": turn, "log": log}
+                content = self.create_contents(words, level_index, turn_index, log_index)
                 self.contents [i] = content
 
-    def analyse_errors(self, line, line_number, file=None):
+    def create_contents(self, words, level_index, turn_index, log_index):
+        if level_index is None:
+            level = "unknown"
+        else:
+            level = words[level_index]
+        if turn_index is None:
+            turn = "unknown"
+        else:
+            turn = words[turn_index]
+        if log_index is None:
+            log = "unknown"
+        else:
+            log = words[log_index]
+        content = {"level": level, "turn": turn, "log": log}
+        return content
+    
+    def analyse_errors(self):
         
         ALLOWED_ERRORS = ["random_choice: Choosed emergency move"]
         if self.contents == {}:
-            try:
-                self.read_log(file)
-            except Exception:
-                raise RuntimeError("Log not read!")
+            raise RuntimeError("Log not read!")
 
         for i , content in self.contents.items():
             level = content.get("level", "unknwon")
-            turn = content.get("turn", "unknwon")
             log = content.get("log", "unknwon")
-
-        self.validate_level(level)
+            self.validate_level(level)
+            self.validate_log(log)
 
         if level == "ERROR":
             pass
@@ -46,6 +52,10 @@ class LogAnalyzer():
     def validate_log(self, log):
         if log == "unknown":
             raise KeyError("To execute the called analyzation the log neeeds a clear entry!")
+        
+    def validate_turn(self, turn):
+        if not isinstance(turn, int):
+            raise KeyError("To execute the called analyzation the log needs clear turns!")
 
     def print_found_errors(self, found_errors):
         for error in found_errors:
