9293411847accb15fd3522a90815f096774891d0
Emil Chatzistamatiou <emil.chatzistamatiou@gmail.com>
2026-03-14 14:18:28 +0100

    TestAnalyseErrors: setUp: created simple patcher list:self.patchers, patcher_output_handler in self.patchers

--- tests/test_tools/test_analyse_errors.py
+++ tests/test_tools/test_analyse_errors.py
@@ -2,7 +2,28 @@ from unittest import TestCase
 from unittest.mock import patch , MagicMock , ANY
 from unittest import main
 from tools.log_analyzer import LogAnalyzer
-class TestAnalyseErrors():
+class TestAnalyseErrors(TestCase):
+
+    log_analyzer = LogAnalyzer()
+    def setUp(self):
+        
+        self.patchers = [
+            patch.object(self.log_analyzer, "output_handler", side_effect=lambda output_format, output: output, name="mock_output_handler")
+        ]
+        self.mocks = {}
+
+        self.start_patchers()
+        self.addCleanup(self.stop_patchers)
+    
+    def start_patchers(self):
+        for patcher in self.patchers:
+            mock = patcher.start()
+            if isinstance(mock, MagicMock):
+                self.mocks [mock._mock_name] = mock
+
+    def stop_patchers(self):
+        for patcher in self.patchers:
+            patcher.stop()
 
     def test_only_allowed_errors(self):
 
