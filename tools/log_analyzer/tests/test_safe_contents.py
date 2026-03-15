74d8dea6f53c6775ac68da0e00229a65492c7338
Emil Chatzistamatiou <emil.chatzistamatiou@gmail.com>
2026-03-16 00:17:50 +0100

    TestSafeContents: test_safe_contents: added

--- tools/log_analyzer/tests/test_safe_contents.py
+++ tools/log_analyzer/tests/test_safe_contents.py
@@ -1,8 +1,25 @@
 from unittest import TestCase
 from unittest.mock import patch , MagicMock , ANY , mock_open
 from unittest import main
+from tools.log_analyzer.log_analyzer import LogAnalyzer
 class TestSafeContents(TestCase):
 
+    file = "..."
+    level_index = "..."
+    turn_index = "..."
+    log_index = "..."
+    log_analyzer = LogAnalyzer(file, level_index, turn_index, log_index)
+    def setUp(self):
+       
+       pass
+    
+    @patch.object(log_analyzer, "contents", new="...")
     def test_safe_contents(self):
 
-        pass
+        mock = mock_open()
+        with patch("builtins.open", mock):
+            self.log_analyzer.save_contents()
+            mock().write.assert_called_once_with("...")
+
+if __name__ == "__main__":
+    main()
