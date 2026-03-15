471bc7dc7af9b405cd4dcb91de4c11dcaa9923b4
Emil Chatzistamatiou <emil.chatzistamatiou@gmail.com>
2026-03-15 23:19:04 +0100

    TestNotBackward: assert_extract_keywords: added args + kwargs into assertion

--- tests/test_move/test_not_backward.py
+++ tests/test_move/test_not_backward.py
@@ -21,7 +21,7 @@ class TestNotBackward(unittest.TestCase):
         if not isinstance(mock, MagicMock):
             raise TypeError(f"Object: {mock} is not MagicMock()")
         
-        mock.assert_called_once_with()
+        mock.assert_called_once_with(["head", "neck"], head={}, neck={})
 
     def extract_moves(self):
         
