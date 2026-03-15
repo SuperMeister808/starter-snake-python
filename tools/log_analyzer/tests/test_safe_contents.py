29c45ffaf6778a9b3280c79d630faf3a0518e711
Emil Chatzistamatiou <emil.chatzistamatiou@gmail.com>
2026-03-16 00:06:04 +0100

    ./tools/log_analyzer/tests/test_safe_contents.py: TestSafeContents created

---
+++ tools/log_analyzer/tests/test_safe_contents.py
@@ -0,0 +1,8 @@
+from unittest import TestCase
+from unittest.mock import patch , MagicMock , ANY , mock_open
+from unittest import main
+class TestSafeContents(TestCase):
+
+    def test_safe_contents(self):
+
+        pass
