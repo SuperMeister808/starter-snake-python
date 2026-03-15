b0e6fb18bb669202a96a4658b1d2cf6d3d712a83
Emil Chatzistamatiou <emil.chatzistamatiou@gmail.com>
2026-03-16 00:29:25 +0100

    TestLoadContents: setUp: patcher self.log_analyzer.read_log

--- tools/log_analyzer/tests/test_load_contents.py
+++ tools/log_analyzer/tests/test_load_contents.py
@@ -2,8 +2,32 @@ from unittest import TestCase
 from unittest.mock import patch , MagicMock , ANY , mock_open
 from unittest import main
 from tools.log_analyzer.log_analyzer import LogAnalyzer
-class TestLoadContents():
+class TestLoadContents(TestCase):
 
+    file = "..."
+    level_index = "..."
+    turn_index = "..."
+    log_index = "..."
+    log_analyzer = LogAnalyzer(file, level_index, turn_index, log_index)
+    def setUp(self):
+        
+        self.mocks = {}
+        self.patchers = [
+            patch.object(self.log_analyzer, "read_log", name="mock_read_log")
+        ]
+
+        self.addCleanup(self.stop_patchers)
+
+    def start_patchers(self):
+        for patcher in self.patchers:
+            mock = patcher.start()
+            if isinstance(mock, MagicMock):
+                self.mocks [mock._mock_name] = mock
+
+    def stop_patchers(self):
+        for patcher in self.patchers:
+            patcher.stop()
+    
     def test_load_valide_contents(self):
 
         pass
