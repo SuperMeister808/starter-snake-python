93c026b9ad94874e7dd1383b78df696fd0bdb2fd
Emil Chatzistamatiou <emil.chatzistamatiou@gmail.com>
2026-03-16 00:21:50 +0100

    ./tools/log_analyzer/tests/test_load_contents.py: TestLoadContents: created

---
+++ tools/log_analyzer/tests/test_load_contents.py
@@ -0,0 +1,13 @@
+from unittest import TestCase
+from unittest.mock import patch , MagicMock , ANY , mock_open
+from unittest import main
+from tools.log_analyzer.log_analyzer import LogAnalyzer
+class TestLoadContents():
+
+    def test_load_valide_contents(self):
+
+        pass
+
+    def test_load_invalid_contents(self):
+
+        pass
