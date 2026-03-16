6c0a52e10738dd9a1863c58bec7ff1bd86a070a8
Emil Chatzistamatiou <emil.chatzistamatiou@gmail.com>
2026-03-16 12:26:49 +0100

    LogAnalyzer: test_read_log: setUp_ patcher self.log_analyzer.save_contents added

--- tools/log_analyzer/tests/test_read_log.py
+++ tools/log_analyzer/tests/test_read_log.py
@@ -1,14 +1,18 @@
 from unittest import TestCase
 from unittest.mock import patch, MagicMock , ANY , mock_open
 from unittest import main
-from tools.log_analyzer import LogAnalyzer
+from tools.log_analyzer.log_analyzer import LogAnalyzer
 class TestReadLog(TestCase):
 
     file = "..."
-    log_analyzer = LogAnalyzer(file)
+    level_index = "..."
+    turn_index = "..."
+    log_index = "..."
+    log_analyzer = LogAnalyzer(file, level_index, turn_index, log_index)
     def setUp(self):
         self.patchers = [
-            patch.object(self.log_analyzer, "create_contents", name="mock_create_contents")
+            patch.object(self.log_analyzer, "create_contents", name="mock_create_contents"),
+            patch.object(self.log_analyzer, "save_contents", name="mock_save_contents")
         ]
         self.mocks = {}
 
@@ -24,36 +28,37 @@ class TestReadLog(TestCase):
     def stop_patcherss(self):
         for pacher in self.patchers:
             pacher.stop()
-    
-    def test_read_log_default_args(self):
-        
-        file_handler = "..."
-        mock_file = mock_open(read_data="HELLO WORLD !")
-        with patch("builtins.open", mock_file):
-            result = self.log_analyzer.read_log(file_handler)
-            mock_create_contents = self.mocks ["mock_create_contents"]
-            expected_words = ["HELLO", "WORLD", "!"]
-            mock_create_contents.assert_called_once_with(expected_words, 0, None, None, None)
 
+    @patch.object(log_analyzer, "level_index", new=0)
+    @patch.object(log_analyzer, "turn_index", new=1)
+    @patch.object(log_analyzer, "log_index", new=2)
     def test_read_log_custom_args(self):
 
         file_handler = "..."
         mock_file = mock_open(read_data="HELLO WORLD !")
         with patch("builtins.open", mock_file):
-            result = self.log_analyzer.read_log(file_handler, 0, 1, 2)
+            result = self.log_analyzer.read_log()
             mock_create_contents = self.mocks ["mock_create_contents"]
             expected_words = ["HELLO", "WORLD", "!"]
             mock_create_contents.assert_called_once_with(expected_words, 0, 0, 1, 2)
+            mock_save_contents = self.mocks ["mock_save_contents"]
+            mock_save_contents.assert_called_once()
 
+    @patch.object(log_analyzer, "file", new={})
+    @patch.object(log_analyzer, "level_index", new=0)
+    @patch.object(log_analyzer, "turn_index", new=1)
+    @patch.object(log_analyzer, "log_index", new=2)
     def test_read_log_unsupported_file_handler(self):
 
         file_handler = 0
         mock_file = mock_open(read_data="HELLO WORLD !")
         with patch("builtins.open", mock_file):
             with self.assertRaises(RuntimeError):
-                result = self.log_analyzer.read_log(file_handler)
+                result = self.log_analyzer.read_log()
                 mock_create_contents = self.mocks ["mock_create_contents"]
                 mock_create_contents.assert_not_called()
+                mock_save_contents = self.mocks ["mock_save_contents"]
+                mock_save_contents.assert_not_called()
 
 if __name__ == "__main__":
     main()
