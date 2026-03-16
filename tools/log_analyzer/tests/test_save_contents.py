05fff6bf394f51320187f8bb2d71a231105c6cc9
Emil Chatzistamatiou <emil.chatzistamatiou@gmail.com>
2026-03-16 14:47:07 +0100

    TestSaveContents: added absolute path into assertion

--- tools/log_analyzer/tests/test_save_contents.py
+++ tools/log_analyzer/tests/test_save_contents.py
@@ -3,7 +3,7 @@ from unittest.mock import patch , MagicMock , ANY , mock_open
 from unittest import main
 from tools.log_analyzer.log_analyzer import LogAnalyzer
 import json
-class TestSafeContents(TestCase):
+class TestSaveContents(TestCase):
 
     file = "..."
     level_index = "..."
@@ -15,13 +15,13 @@ class TestSafeContents(TestCase):
        pass
     
     @patch.object(log_analyzer, "contents", new=[])
-    def test_safe_contents(self):
+    def test_save_contents(self):
 
         mock = mock_open()
         with patch("builtins.open", mock):
             contents_json = json.dumps(self.log_analyzer.contents)
             self.log_analyzer.save_contents()
-            mock.assert_called_once_with("ressources/contents.json", "w")
+            mock.assert_called_once_with(r'C:\Users\emilc\game_agent\starter-snake-python\tools\log_analyzer\ressources\contents.json', 'w')
             mock().write.assert_called_once_with(contents_json)
 
 if __name__ == "__main__":
