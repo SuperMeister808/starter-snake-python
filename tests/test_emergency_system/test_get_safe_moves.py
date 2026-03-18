eb3f9eaa822044f5520a809a73a463b70dc9e8d8
Emil Chatzistamatiou <emil.chatzistamatiou@gmail.com>
2026-03-18 21:08:26 +0100

    TestGetSafeMoves: import error fix

--- tests/test_emergency_system/test_get_safe_moves.py
+++ tests/test_emergency_system/test_get_safe_moves.py
@@ -3,7 +3,7 @@ import unittest
 from unittest.mock import patch , MagicMock
 
 from move import Move
-from emergency_logger import EmergencyLogger
+from logger.emergency_logger import EmergencyLogger
 
 class TestGetSafeMoves(unittest.TestCase):
     
