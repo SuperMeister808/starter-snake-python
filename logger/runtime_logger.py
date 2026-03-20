1c758c79b7ae35bc3b1d14301840bc554a4e896b
Emil Chatzistamatiou <emil.chatzistamatiou@gmail.com>
2026-03-20 21:31:02 +0100

    refactor: simplified to tenerary extensions + add comments

--- logger/runtime_logger.py
+++ logger/runtime_logger.py
@@ -2,57 +2,53 @@
 import logging
 from logging import LoggerAdapter
 
-class RuntimeLogger():
+# Base logger class that sets up and manages the runtime file logger.
+# EmergencyLogger builds on this class for async logging support.
+class RuntimeLogger:
 
     @classmethod
     def setup(cls, logger_name, file, debug):
+        # creates the logger and attaches a file handler
         logger = cls.create_logger(logger_name, debug)
         cls.add_file_handler(logger, file)
-    
-    @classmethod
-    def create_logger(cls, logger, debug):
 
-        logger = logging.getLogger(logger)
-        if debug:
-            logger.setLevel(logging.DEBUG)
-        else:
-            logger.setLevel(logging.INFO)
+    @classmethod
+    def create_logger(cls, logger_name, debug):
+        # sets log level to DEBUG if debug mode is enabled, otherwise INFO
+        logger = logging.getLogger(logger_name)
+        logger.setLevel(logging.DEBUG if debug else logging.INFO)
         return logger
 
     @classmethod
     def add_file_handler(cls, logger, file):
+        # only adds a file handler if one doesn't already exist
         if not any(isinstance(h, logging.FileHandler) for h in logger.handlers):
             file_handler = logging.FileHandler(file, delay=True)
-        
-            formatter = cls.create_runtime_log_formatter()
-            file_handler.setFormatter(formatter)
+            file_handler.setFormatter(cls.create_runtime_log_formatter())
             logger.addHandler(file_handler)
-        
+
     @classmethod
     def create_runtime_log_formatter(cls):
+        # formats log entries as "LEVEL | TURN X | message"
+        return logging.Formatter("%(levelname)s | TURN %(turn)s | %(message)s")
 
-        formatter = logging.Formatter(
-            "%(levelname)s | TURN %(turn)s | %(message)s"
-        )
-        return formatter
-    
     @staticmethod
     def close_file_handlers(logger):
+        # closes and removes all handlers from the logger after the game ends
         if not isinstance(logger, logging.Logger):
-            raise RuntimeError("Kein logger Objekt übergeben!")
-        
+            raise RuntimeError("No logger object provided!")
         try:
             for handler in logger.handlers[:]:
                 handler.close()
                 logger.removeHandler(handler)
         except AttributeError:
-            raise RuntimeError("Keine Handler vorhanden!")    
+            raise RuntimeError("No handlers found!")   
 
+# Ensures every log entry has a turn value even if none was provided.
 class DefaultTurnAdapter(LoggerAdapter):
 
     def process(self, msg, **kwargs):
-
         extra = kwargs.get("extra", {})
         extra.setdefault("turn", "unknown")
         kwargs["extra"] = extra
-        return msg , kwargs
+        return msg, kwargs
